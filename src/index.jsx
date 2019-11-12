8f253ac2bf4cba06431b0040421bee810d5d2212
Alexander Kasperovich <AlexanderKasperovich@coherentsolutions.com>
2019-11-12 17:24:36 +0300

    feat: Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const instate = ['Kas', 'Alex'];
+
+function playlist(state = instate, action) {
+    if (action.type === 'ADD_TRACK') {
+      return [
+        ...state,
+        action.payload
+      ];
+    }
+    return state;
+  }
+  
+  const store = createStore(playlist);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('app'));
