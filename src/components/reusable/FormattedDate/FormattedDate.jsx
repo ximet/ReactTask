465b9f49a18d51884b78638ec711bfa11b6e3ca4
Dmitry Goman <DmitryGoman@coherentsolutions.com>
2019-12-17 15:33:49 +0300

    refactoring

--- src/components/reusable/FormattedDate/FormattedDate.jsx
+++ src/components/reusable/FormattedDate/FormattedDate.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const getData = async () => {
-    const forecastRequest = new Request('https://api.darksky.net/forecast/38c286921dca0454c6cc71ad35a06ef0/53.893009,27.567444');
-    const response = await fetch(forecastRequest);
+const getData = async () => {    
+    // const response = await fetch('https://api.darksky.net/forecast/38c286921dca0454c6cc71ad35a06ef0/53.893009,27.567444');
+    const response = await fetch('http://api.openweathermap.org/data/2.5/group?id=625144,524901,2950158&units=metric&APPID=9bd16a665dc2bddd94bbed96d24ca638');
+    
     console.log(response);
-    const forecastData = await response.json();
-    console.log(forecastData);
+    // const forecastData = await response.json();
+    // console.log(forecastData);
 }
 
 const FormattedDate = (props) => (
