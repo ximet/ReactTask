336402a1085f3baaf0fb27d6584e35ba4e5c8451
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-03-10 18:26:32 +0200

    first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import styles from './App.module.css';
 
 class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { buttonColor: 'red' };
+    this.handleCLick = this.handleCLick.bind(this);
+  }
+
+  handleCLick() {
+    this.setState((state) => {
+      const color = state.buttonColor === 'red' ? 'blue' : 'red';
+      return { buttonColor: color };
+    });
+  }
+
   render() {
-    return <div>Hello world!</div>;
+    const { buttonColor } = this.state;
+    return (
+      <div className={styles.container}>
+        <button type="button" onClick={this.handleCLick} className={`${styles.resize} ${styles[buttonColor]}`}>Click me</button>
+      </div>
+    );
   }
 }
 
