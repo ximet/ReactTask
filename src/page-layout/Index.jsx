5665167fba3dccb1e62f24b8342e9b01a3f593ee
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-03-16 11:03:17 +0200

    second_task

--- src/page-layout/Index.jsx
+++ src/page-layout/Index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Index';
 import Footer from '../components/Footer/Index';
 
+// eslint-disable-next-line react/prop-types
 function PageLayout({ children }) {
   return (
     <>
