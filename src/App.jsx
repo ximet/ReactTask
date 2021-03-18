e2a1cd902eb27ae5cd400095dedf8bf7c7eb5ba7
av29 <snumber29@gmail.com>
2021-03-18 21:48:10 +0300

    reformat

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class App extends React.PureComponent {
-    render() {
-        return <div>Hello world!</div>;
-    }
+  render() {
+    return <div>Hello world!</div>;
+  }
 }
 
 export default App;
