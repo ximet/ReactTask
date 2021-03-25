a7f83ea34ae53e6598ab5f4906c4167211a28b97
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-03-25 16:34:13 +0200

    adding NodeJsServer

--- NodeJsProxy/index.js
+++ NodeJsProxy/index.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const request = require('request');
-require('dotenv').config()
+require('dotenv').config();
 
 // Create Express Server
 const app = express();
 
 // Configuration
 const PORT = 3000;
-const HOST = "localhost";
+const HOST = 'localhost';
 
 app.use(cors());
 
-app.get('/auth', function(req,res) {
-
-    var url = `https://pfa.foreca.com/authorize/token?user=${process.env.USER}&password=${process.env.PASS}&expire_hours=${process.env.EXPIRE_HOURS}`;
-    request(url).pipe(res);
-  });
+app.get('/auth', function (req, res) {
+  var url = `https://pfa.foreca.com/authorize/token?user=${process.env.USER}&password=${process.env.PASS}&expire_hours=${process.env.EXPIRE_HOURS}`;
+  request(url).pipe(res);
+});
 
 // Start Proxy
 app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
 });
