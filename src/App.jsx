e12def59f996d4633943c5bc8c7bf77cd42d3eb5
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-04-19 11:50:31 +0300

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Navigation from './Navigation/Navigation';
-import store from './ReduxStore/store/index';
+import store from './ReduxStore/store';
 
 function App() {
   return (
