4fec438c969b08b778d64e101c0764cbbdfd783a
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-04-22 13:51:25 +0300

    Move Css variables in JS file

---
+++ src/common/cssVariables.js
@@ -0,0 +1,34 @@
+export const blue_light = 'rgba(173, 216, 230, 1)';
+export const grey_dark = 'rgba(128, 128, 128, 1)';
+export const blue_medium = 'rgba(51, 102, 255, 0.5)';
+export const red_dark = 'rgba(255, 0, 0, 1)';
+export const blue_dark = 'rgba(0, 0, 255, 1)';
+export const black_light = 'rgba(51, 51, 51, 1)';
+export const grey_light = 'rgba(107, 128, 150, 1)';
+export const white = 'rgba(	250, 250, 250, 1)';
+export const temperatureTextColor = red_dark;
+export const windTextColor = blue_dark;
+export const buttonBackgroundColorLight = blue_medium;
+export const buttonBackgroundColorDark = grey_light;
+export const footerBackgroundColorLight = blue_light;
+export const footerBackgroundColorDark = grey_dark;
+export const headerBackgroundColorLight = blue_light;
+export const headerBackgroundColorDark = grey_dark;
+export const filterableListBackgroundColorLight = blue_light;
+export const filterableListBackgroundColorDark = grey_dark;
+export const aTextColorLightTheme = black_light;
+export const aTextColorDarkTheme = white;
+export const textColorDarkTheme = white;
+export const textColorLightTheme = black_light;
+export const toggleBorderLight = white;
+export const toggleBorderDark = grey_light;
+export const mainViewLight = white;
+export const mainViewDark = black_light;
+
+//height
+export const headerHeight = '6.25em';
+export const footerHeight = '6.25em';
+export const filterableListHeight = '48em';
+
+//width
+export const filterableListWidth = '12em';
