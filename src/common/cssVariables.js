c69620c038d6a6d3fb84ab6090788715e791e1b9
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-04-23 11:59:51 +0300

    made contasts UpperCased

--- src/common/cssVariables.js
+++ src/common/cssVariables.js
@@ -1,4 +1,4 @@
-export const colors = {
+export const COLORS = {
   blue_light: 'rgba(173, 216, 230, 1)',
   blue_dark: 'rgba(0, 0, 255, 1)',
   blue_medium: 'rgba(51, 102, 255, 0.5)',
@@ -6,13 +6,13 @@ export const colors = {
   grey_light: 'rgba(107, 128, 150, 1)',
   red: 'rgba(255, 0, 0, 1)',
   black_light: 'rgba(51, 51, 51, 1)',
-  white: 'rgba(	250, 250, 250, 1)'
+  white: 'rgba(250, 250, 250, 1)'
 };
 
 //height
-export const headerHeight = '6.25em';
-export const footerHeight = '6.25em';
-export const filterableListHeight = '48em';
+export const HEADER_HEIGHT = '6.25em';
+export const FOOTER_HEIGHT = '6.25em';
+export const FILTERABLE_LIST_HEIGHT = '48em';
 
 //width
-export const filterableListWidth = '12em';
+export const FILTERABLE_LIST_WIDTH = '12em';
