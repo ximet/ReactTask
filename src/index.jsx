e02fb0e9d53c9b70edf5e54f10d592c0765d4c51
MartinYDimitrov <MartinDimitrov@coherentsolutions.com>
2021-04-23 18:18:35 +0300

    delete unnecessary files

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import styles from './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
