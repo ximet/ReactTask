f2deb79a82fdb74dcd457ce7b193198bcba5828a
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-09-22 13:29:16 +0300

    changed names of veriables like(WE_API_URL to WEATHER_API_URL), changed name of a start function

--- client/src/components/navLink/NavLink.jsx
+++ client/src/components/navLink/NavLink.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './navLink.module.css'
 
-function NavLink() {
-  return <div></div>;
+function NavLink({path, text}) {
+  return (
+    <Link className={classes.mSize, classes.link} to={path}>
+      {text}
+    </Link>
+  )
 }
 
 export default NavLink;
