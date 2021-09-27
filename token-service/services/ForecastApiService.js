5386af0312a1ec1d4d2091e0b884769ab795d42d
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-09-27 16:58:46 +0300

    Rewriting of methods for token getting.

--- token-service/services/ForecastApiService.js
+++ token-service/services/ForecastApiService.js
@@ -1,67 +1,37 @@
-const { Curl } = require('node-libcurl');
+const axios = require('axios');
 
 const ForecastApiService = {
-  getToken: function (response) {
-    const curlRequest = new Curl();
-    const terminate = curlRequest.close.bind(curlRequest);
-    const context = this;
+  createRequestInstance: async function () {
+    return await axios.create({
+      baseURL: process.env.FORECAST_API_DOMAIN
+    });
+  },
 
-    const domainApi = process.env.FORECAST_API_DOMAIN;
+  getToken: async function (response) {
+    let resonseData = {
+      status: false
+    };
 
-    curlRequest.setOpt(Curl.option.URL, `${domainApi}/authorize/token`);
-    curlRequest.setOpt(Curl.option.POST, true);
-    curlRequest.setOpt(Curl.option.VERBOSE, true);
-    curlRequest.setOpt(Curl.option.SSL_VERIFYPEER, false);
-    curlRequest.setOpt(
-      Curl.option.POSTFIELDS,
-      JSON.stringify({
+    try {
+      const requestInstance = await this.createRequestInstance();
+
+      const { data } = await requestInstance.post('/authorize/token', {
         user: process.env.FORECAST_API_USER,
         password: process.env.FORECAST_API_PSWD
-      })
-    );
+      });
 
-    curlRequest.on('end', function (statusCode, data, headers) {
-      console.log('headers: ', headers);
-      context.curlRequestEnd(data, response);
-      this.close();
-    });
-
-    curlRequest.on('error', error => {
-      context.curlRequestError(error, response, terminate);
-    });
-
-    curlRequest.perform();
-  },
-
-  curlRequestEnd: (data, response) => {
-    console.info('response: ' + data);
-    try {
-      const accessData = JSON.parse(data);
-      const { access_token } = accessData;
+      const { access_token, expires_in } = data;
 
       response.cookie('token', access_token, {
-        maxAge: accessData.expires_in * 1000
+        maxAge: expires_in * 1000
       });
+
+      resonseData.status = true;
     } catch (error) {
-      console.error('test', error);
+      console.error(error);
     }
 
-    response.send(
-      JSON.stringify({
-        status: true
-      })
-    );
-  },
-
-  curlRequestError: (error, response, terminate) => {
-    console.error('error: ', error);
-
-    terminate();
-    response.send(
-      JSON.stringify({
-        status: true
-      })
-    );
+    response.send(JSON.stringify(resonseData));
   }
 };
 
