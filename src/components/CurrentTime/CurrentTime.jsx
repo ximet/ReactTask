4e0dae6004f15c10b99adb04fc765849be2f4d87
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-09-30 13:06:05 +0300

    Creating current time component.

--- src/components/CurrentTime/CurrentTime.jsx
+++ src/components/CurrentTime/CurrentTime.jsx
@@ -1,7 +1,22 @@
 import classes from './CurrentTime.module.scss';
+import { useState, useEffect } from 'react';
 
 function CurrentTime() {
-  return <div className={classes.currentTime}>17:00</div>;
+  const [currentTime, setCurrentTime] = useState(new Date().toTimeString());
+
+  useEffect(() => {
+    setTimeout(() => {
+      const dateObj = new Date();
+      const hours = dateObj.getHours().toString().padStart(2, 0);
+      const minutes = dateObj.getMinutes().toString().padStart(2, 0);
+      const seconds = dateObj.getSeconds().toString().padStart(2, 0);
+      const timeString = `${hours}:${minutes}:${seconds}`;
+
+      setCurrentTime(timeString);
+    }, 1000);
+  }, [currentTime]);
+
+  return <div className={classes.currentTime}>{currentTime}</div>;
 }
 
 export default CurrentTime;
