9b17252c2fb1e8dd529b2240e7fe76caa72ac76e
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-09-30 12:05:07 +0300

    cnanged function findMatchingCities and removed index from key in Cities component

--- client/src/layouts/header/searchForm/SearchForm.jsx
+++ client/src/layouts/header/searchForm/SearchForm.jsx
@@ -7,32 +7,25 @@ import { cities } from '../../../../arrayOfCities';
 function SearchForm() {
   const [searchText, setSearchText] = useState('');
   const [matchingCities, setMatchingCities] = useState([]);
-  const [needToShowMatchingCities, setNeedToShowMatchingCities] = useState(false);
-
-  console.log(matchingCities);
 
   const handleSearchText = e => {
     setSearchText(e.target.value);
-    console.log(e.target.value);
   };
 
-  const findmatchingCities = () => {
-    setMatchingCities(
-      cities.filter(city => {
-        return city.name.toLowerCase().startsWith(searchText.toLowerCase());
-      })
-    );
-    matchingCities.length ? setNeedToShowMatchingCities(true) : setNeedToShowMatchingCities(false);
+  const findMatchingCities = () => {
+    const tempMatchingCities = searchText !== '' ? cities.filter(city => city.name.toLowerCase().startsWith(searchText.toLowerCase())) : [];
+
+    setMatchingCities(tempMatchingCities);
   };
 
   useEffect(() => {
-    findmatchingCities();
+    findMatchingCities();
   }, [searchText]);
 
   return (
     <div className={classes.container}>
       <SearchBar searchText={searchText} handleSearchText={handleSearchText} />
-      {needToShowMatchingCities && <Cities cities={matchingCities} />}
+      {matchingCities.length > 0 && <Cities cities={matchingCities} />}
     </div>
   );
 }
--- client/src/layouts/header/searchForm/cities/Cities.jsx
+++ client/src/layouts/header/searchForm/cities/Cities.jsx
@@ -5,8 +5,8 @@ import City from './city/City';
 function Cities({ cities }) {
   return (
     <div className={classes.container}>
-      {cities.map((city, index) => (
-        <City key={`${city}${index}`} city={city} />
+      {cities.map((city) => (
+        <City key={city.name} city={city} />
       ))}
     </div>
   );
