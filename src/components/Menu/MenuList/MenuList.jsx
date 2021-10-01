063ca10c17af466146085a2746e53175c5b94d31
Oleksandr Viktorov <oleksandrviktorov@coherentsolutions.com>
2021-10-01 09:14:53 +0300

    [ReactTask][mian_page_components] constants folder added

--- src/components/Menu/MenuList/MenuList.jsx
+++ src/components/Menu/MenuList/MenuList.jsx
@@ -1,11 +1,12 @@
+import { CITIES_PAGE_LINK, CONTACT_US_PAGE_LINK } from '../../../constants/constants';
 import MenuItem from '../MenuItem/MenuItem';
-import './MenuList.css'
+import './MenuList.css';
 
 function MenuList() {
   return (
     <ul className="menu__list">
-      <MenuItem text="About Us" link="/aboutus" />
-      <MenuItem text="Cities" link="/cities" />
+      <MenuItem text="About Us" link={CONTACT_US_PAGE_LINK} />
+      <MenuItem text="Cities" link={CITIES_PAGE_LINK} />
     </ul>
   );
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const THEME_LIGHT = 'light';
+export const THEME_DARK = 'dark';
+
+export const CONTACT_US_PAGE_LINK = '/aboutus';
+export const CITIES_PAGE_LINK = '/cities';
