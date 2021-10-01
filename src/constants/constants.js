0058929123a5b812f2f55dc36c031c7637a87b39
Oleksandr Viktorov <oleksandrviktorov@coherentsolutions.com>
2021-10-01 16:48:42 +0300

    [ReactTask][data_service] getData function created and other weatherAPI functios refactored

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -10,6 +10,27 @@ export const API_BASE_URL = '/api'; //proxed to 'https://pfa.foreca.com' via web
 export const API_AUTH_URL = '/authorize/token';
 export const API_SEARCH_URL = '/api/v1/location/search/';
 export const API_CURRENT_WEATHER_URL = '/api/v1/current/';
+export const API_FORECAST_URL = '/api/v1/forecast/';
+export const API_FORECAST_3_HOURLY = '3hourly/';
+export const API_FORECAST_DAILY = 'daily/';
 
+export const API_SEARCH_LOCATION_DATA_TYPE = 'searchLocation';
+export const API_GET_CURRENT_WEATHER_DATA_TYPE = 'getCurrentWeather';
+export const API_GET_FORECAST_DATA_TYPE = 'getForecast';
+
+export const DATA_TYPES = {
+  [API_SEARCH_LOCATION_DATA_TYPE]: {
+    url: API_SEARCH_URL,
+    dataKey: 'locations'
+  },
+  [API_GET_CURRENT_WEATHER_DATA_TYPE]: {
+    url: API_CURRENT_WEATHER_URL,
+    dataKey: 'current'
+  },
+  [API_GET_FORECAST_DATA_TYPE]: {
+    url: API_FORECAST_URL,
+    dataKey: 'forecast'
+  }
+};
 
 export const API_KIEV_ID = 100703448;
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,63 +1,67 @@
 import * as axios from 'axios';
 import {
-    API_AUTH_URL,
-    API_BASE_URL,
-    API_CURRENT_WEATHER_URL,
-    API_SEARCH_URL
+  API_AUTH_URL,
+  API_BASE_URL,
+  DATA_TYPES,
+  API_SEARCH_LOCATION_DATA_TYPE,
+  API_GET_CURRENT_WEATHER_DATA_TYPE,
+  API_GET_FORECAST_DATA_TYPE
 } from '../constants/constants';
 
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: API_BASE_URL
+  withCredentials: true,
+  baseURL: API_BASE_URL
 });
 
 export const weatherAPI = {
-    token: null,
-    getAuthHeaders() {
-        return {
-            headers: {
-                Authorization: `Bearer ${this.token}`
-            }
+  token: null,
+  getAuthHeaders() {
+    return {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    };
+  },
+  async getToken(user, password) {
+    const body = {
+      user: `${user}`,
+      password: `${password}`
+    };
+    try {
+      const response = await instance.post(API_AUTH_URL, body, {
+        headers: {
+          'Content-Type': 'text/plain'
         }
-    },
-    async getToken(user, password) {
-        const body = {
-            user: `${user}`,
-            password: `${password}`
-        };
-        try {
-            const response = await instance.post(API_AUTH_URL, body, {
-                headers: {
-                    'Content-Type': 'text/plain'
-                }
-            });
-            this.token = response.data.access_token;
-        } catch (error) {
-            console.log(error);
-            this.token = null;
-        }
-
-        return this.token;
-    },
-    async searchLocation(query) {
-        try {
-            const response = await instance.get(API_SEARCH_URL + query, this.getAuthHeaders());
-            return response.data.locations;
-        } catch (error) {
-            console.log(error);
-            return null;
-        }
-    },
-    async getCurrentWeather(locationId) {
-        try {
-            const response = await instance.get(API_CURRENT_WEATHER_URL + locationId, this.getAuthHeaders());
-            return response.data.current;
-        } catch (error) {
-            console.log(error);
-            return null;
-        }        
-    },
-    getForecast(forecastType, locationId) {
-        return instance.get(API_SEARCH_URL + query, this.getAuthHeaders());
+      });
+      this.token = response.data.access_token;
+    } catch (error) {
+      console.log(error);
+      this.token = null;
     }
+
+    return this.token;
+  },
+
+  async getData(dataType, param) {
+    const { url, dataKey } = DATA_TYPES[dataType];
+    try {
+      const response = await instance.get(url + param, this.getAuthHeaders());
+      return response.data[dataKey];
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  async searchLocation(query) {
+    return this.getData(API_SEARCH_LOCATION_DATA_TYPE, query);
+  },
+
+  async getCurrentWeather(locationId) {
+    return this.getData(API_GET_CURRENT_WEATHER_DATA_TYPE, locationId);
+  },
+
+  async getForecast(forecastType, locationId) {
+    return this.getData(API_GET_FORECAST_DATA_TYPE, forecastType + locationId);
+  }
 };
