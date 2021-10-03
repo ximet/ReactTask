a91e2781f74aafea80c878cdc9de1c8f962dbf75
Nastya <asekerich99@gmail.com>
2021-10-03 15:04:40 +0300

    feat: add basic pages files

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import style from './Main.module.scss';
 
+import WeatherForecast from '../../views/WeatherForecast/WeatherForecast';
+
 function Main() {
-  return <div className={style.main}>Main</div>;
+  return (
+    <div className={style.main}>
+      <WeatherForecast />
+    </div>
+  );
 }
 
 export default Main;
