038723f845b9ab030f636a8a0e943278da5057c2
Oleksandr Viktorov <oleksandrviktorov@coherentsolutions.com>
2021-10-04 22:21:39 +0300

    [ReactTask][data_service] fixes from PR comments are done

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,14 @@ import {
 import { weatherAPI } from './services/dataService';
 
 function App() {
-  weatherAPI.getToken(API_AUTH_USERNAME, API_AUTH_PASS).then(() => {
-    weatherAPI.searchLocation('Kiev').then(locations => console.log(locations));
-    weatherAPI.getCurrentWeather(API_KIEV_ID).then(weather => console.log(weather));
-    weatherAPI
-      .getForecast(API_FORECAST_3_HOURLY, API_KIEV_ID)
-      .then(forecast => console.log(forecast));
-    weatherAPI.getForecast(API_FORECAST_DAILY, API_KIEV_ID).then(forecast => console.log(forecast));
-  });
+  // weatherAPI.getToken(API_AUTH_USERNAME, API_AUTH_PASS).then(() => {
+  //   weatherAPI.searchLocation('Kiev').then(locations => console.log(locations));
+  //   weatherAPI.getCurrentWeather(API_KIEV_ID).then(weather => console.log(weather));
+  //   weatherAPI
+  //     .getForecast(API_FORECAST_3_HOURLY, API_KIEV_ID)
+  //     .then(forecast => console.log(forecast));
+  //   weatherAPI.getForecast(API_FORECAST_DAILY, API_KIEV_ID).then(forecast => console.log(forecast));
+  // });
 
   const [theme, setTheme] = useState(THEME_LIGHT);
 
