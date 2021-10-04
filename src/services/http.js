d32ec89487607ec2d1d8594fae36fb732ffb1307
ryankovich <ryankovich@aristeksystems.com>
2021-10-04 18:12:42 +0300

    refactor: remove unused header

--- src/services/http.js
+++ src/services/http.js
@@ -3,9 +3,5 @@ import axios from 'axios';
 const http = axios.create();
 
 http.defaults.baseURL = process.env.BASE_URL;
-http.defaults.headers = {
-  // TODO: change to correct
-  'Access-Control-Allow-Origin': '*',
-};
 
 export default http;
