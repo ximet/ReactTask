e87c639f14b6e374f1466ea96910522c543125fd
ryankovich <ryankovich@aristeksystems.com>
2021-10-06 17:30:05 +0300

    refactor: setup configs

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function (api) {
       },
     ],
     ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
   ];
   const plugins = [
     '@babel/transform-react-constant-elements',
