a70f99d56cec9d35175451c4a2d0146982c9a575
Nastya <asekerich99@gmail.com>
2021-10-06 15:54:45 +0300

    feat: add data to cityForecast

--- src/components/CurrentDate/CurrentDate.jsx
+++ src/components/CurrentDate/CurrentDate.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
-function CurrentDate() {
-  return <div>Date</div>;
+import styles from './CurrentDate.module.scss';
+import { FORECAST_PATHS } from '../../constants/constants';
+
+function CurrentDate({ weatherImg }) {
+  return (
+    <div className={styles.currentDate}>
+      <img
+        src={`${FORECAST_PATHS.getIcon}${weatherImg}.png`}
+        alt="weather icon"
+        className={styles.weatherIcon}
+      />
+      <div className={styles.dateInfo}>
+        <span className={styles.time}>12:44 PM</span>
+        <span className={styles.date}>Fri, 21 Sep</span>
+      </div>
+    </div>
+  );
 }
 
 export default CurrentDate;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,12 +5,13 @@ export const FORECAST_PATHS = {
   getCityInfoUrl: '/api/v1/location/',
   getCurrentWeatherUrl: '/api/v1/current/',
   getForecastUrl: '/api/v1/forecast/',
-  getForecastUrl: '/api/v1/forecast/'
+  getForecastUrl: '/api/v1/forecast/',
+  getIcon: 'https://developer.foreca.com/static/images/symbols/'
 };
 
 export const FORECAST_TYPES = {
   daily: 'daily/',
   hourly: 'hourly/'
-}
+};
 
 export const CURRENT_CITY_ID = 100625144;
