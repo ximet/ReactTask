96ee150508ef37f386e7d46648478b0c86657708
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-07 16:43:58 +0300

    Refactoring of redux setup.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
 const configureStore = (initialState = {}) => {
-    return createStore(rootReducer, initialState);
-}
+  return createStore(rootReducer, initialState);
+};
 
 export default configureStore;
