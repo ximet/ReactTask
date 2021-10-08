388f302f6009eb35e822687e3b045ac782a1c28e
Nastya <asekerich99@gmail.com>
2021-10-08 12:53:39 +0300

    feat: add basic styles for search input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import styles from './SearchInput.module.scss';
+import SearchIcon from '../../assets/images/search-icon.png';
+
 function SearchInput() {
-  return <div>Search input</div>;
+  return (
+    <>
+      <div className={styles.searchWrapper}>
+        <input id="searchInput" type="text" placeholder="Search" className={styles.searchInput} />
+        <label htmlFor="searchInput" className={styles.searchInputLabel}>
+          <img src={SearchIcon} alt="search icon" />
+        </label>
+      </div>
+      <div className={styles.searchResults} />
+    </>
+  );
 }
 
 export default SearchInput;
