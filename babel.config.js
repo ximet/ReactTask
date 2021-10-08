2fcb33e8ed181b1a61f5c3b0292bafc022a74c7f
alvikalvik <alvikalvik@gmail.com>
2021-10-08 09:55:13 +0300

    [ReactTask][master] babel.config.js file modified for prop-types proper work (#69)
    Co-authored-by: Oleksandr Viktorov <oleksandrviktorov@coherentsolutions.com>

--- babel.config.js
+++ babel.config.js
@@ -16,8 +16,7 @@ module.exports = function (api) {
     ['@babel/preset-react', { runtime: 'automatic' }]
   ];
   const plugins = [
-    '@babel/transform-react-constant-elements',
-    'transform-react-remove-prop-types',
+    '@babel/transform-react-constant-elements',    
     'transform-react-pure-class-to-function',
     '@babel/plugin-transform-runtime',
     'react-hot-loader/babel',
