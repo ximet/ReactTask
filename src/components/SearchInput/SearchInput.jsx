f914d214fe3df138dd089bd27f1523296c95f225
Nastya <asekerich99@gmail.com>
2021-10-08 16:18:19 +0300

    feat: add styles and search cities

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,19 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styles from './SearchInput.module.scss';
 import SearchIcon from '../../assets/images/search-icon.png';
+import SearchLocationIcon from '../../assets/images/search-location-icon.png';
+import { NUMBER_OF_RESULTS } from '../../constants/forecaApi';
+import { dataService } from '../../services/dataService';
 
 function SearchInput() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (searchQuery) {
+      searchCities();
+    }
+  }, [searchQuery]);
+
+  async function searchCities() {
+    try {
+      const { locations } = await dataService.searchCity(searchQuery);
+
+      locations.splice(NUMBER_OF_RESULTS);
+      setSearchResults(locations);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  };
+
+  const renderSearchResults = searchResults.map(item => (
+    <li key={item.id} className={styles.searchResultItem}>
+      <img src={SearchLocationIcon} alt="location" />
+      <span>{item.name}</span>
+    </li>
+  ));
+
   return (
-    <>
-      <div className={styles.searchWrapper}>
-        <input id="searchInput" type="text" placeholder="Search" className={styles.searchInput} />
+    <div className={styles.searchWrapper}>
+      <div className={styles.searchForm}>
+        <input
+          id="searchInput"
+          type="text"
+          placeholder="Search"
+          autoComplete="off"
+          value={searchQuery}
+          className={styles.searchInput}
+          onChange={handleChange}
+        />
         <label htmlFor="searchInput" className={styles.searchInputLabel}>
           <img src={SearchIcon} alt="search icon" />
         </label>
       </div>
-      <div className={styles.searchResults} />
-    </>
+      <div className={styles.searchResults}>{renderSearchResults}</div>
+    </div>
   );
 }
 
