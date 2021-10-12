23879514a42f43aa7ca3409e91dd9f7b3b08f70b
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-12 18:02:02 +0300

    Setup react router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import CurrentCityForecastView from './views/CurrentCityForecastView/CurrentCityForecastView';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <CurrentCityForecastView />
+      <Switch>
+        <Route path="/">
+          <CurrentCityForecastView />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
