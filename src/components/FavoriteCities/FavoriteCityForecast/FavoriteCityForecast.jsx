ad30bc18055274b496c40f5a86e848167c9cee52
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-12 17:05:16 +0300

    Adding api service methods.

--- src/components/FavoriteCities/FavoriteCityForecast/FavoriteCityForecast.jsx
+++ src/components/FavoriteCities/FavoriteCityForecast/FavoriteCityForecast.jsx
@@ -1,23 +1,30 @@
+import { useState, useEffect } from 'react';
 import classes from './FavoriteCityForecast.module.scss';
+import ApiService from '../../../services/ForecastApiService';
+import { useCookies } from 'react-cookie';
 
 function FavoriteCityForecast({ location }) {
+  const [forecast, setForecast] = useState({});
+  const [cookies] = useCookies(['token']);
+
+  useEffect(async () => {
+    const { current } = await ApiService.getCurrentForecast(location.id, cookies);
+    setForecast(current);
+  }, []);
+
+  const symbolUrl = `https://developer.foreca.com/static/images/symbols/${forecast.symbol}.png`;
   return (
     <div className={classes.item}>
       <div className={classes.itemInfo}>
         <div className={classes.mainInfo}>
-          <img
-            className={classes.icon}
-            src="https://developer.foreca.com/static/images/symbols/d310.png"
-            alt="forecast"
-            title="forecast"
-          />
-          <div className={classes.temperature}>20</div>
+          <img className={classes.icon} src={symbolUrl} alt="forecast" title="forecast" />
+          <div className={classes.temperature}>{forecast.temperature}</div>
         </div>
         <div className={classes.additionalInfo}>
           <div className={classes.cityName}>{location.name}</div>
-          <div className={classes.wind}>Wind: 1 km/h</div>
-          <div className={classes.humidity}>Humidity: 30%</div>
-          <div className={classes.precitipate}>Precitipate: 0%</div>
+          <div className={classes.wind}>Wind: {forecast.windSpeed} km/h</div>
+          <div className={classes.humidity}>Humidity: {forecast.relHumidity}%</div>
+          <div className={classes.precitipate}>Precitipate: {forecast.precipProb}%</div>
           <a className={classes.linkMore} href="#">
             read more
           </a>
