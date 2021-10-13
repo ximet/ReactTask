11d5420a27b46d465112e2987500f39c0ac7cb1e
Nastya <asekerich99@gmail.com>
2021-10-13 11:58:06 +0300

    feat: add theme reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 import locationReducer from '../reducers/locationReducer';
+import themeReducer from '../reducers/themeReducer';
 
-const rootReducer = combineReducers({ locationReducer });
+const rootReducer = combineReducers({ locationReducer, themeReducer });
 
 export default rootReducer;
