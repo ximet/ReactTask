32f25ead069ab4d8ec23856501c9117fdf1bb499
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-14 15:02:11 +0300

    removed unnecessary imports from AppContainer

--- client/src/AppContainer.js
+++ client/src/AppContainer.js
@@ -1,9 +1,5 @@
 import { connect } from 'react-redux';
 import App from './App';
-import { getLocationAction } from './redux/actions/locationAction';
-import { getWeatherAction } from './redux/actions/currentWeatherActions';
-import { getHourlyWeatherAction } from './redux/actions/hourlyWeatherAction';
-import { getDailyWeatherAction } from './redux/actions/dailyWeatherActions';
 import { initializeApp } from './redux/actions/appInitializationAction';
 
 const mapStateToProps = (state, ownProps) => ({
