a5e6e2962b1f1e3dae6f98e4e016cd0271cf6d92
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-14 16:18:17 +0300

    Refactoring

--- src/components/CurrentCityForecast/components/DailyForecasts/DailyForecast/DailyForecast.jsx
+++ src/components/CurrentCityForecast/components/DailyForecasts/DailyForecast/DailyForecast.jsx
@@ -1,17 +1,28 @@
 import classes from './DailyForecast.module.scss';
-import { getDayShort } from '../../../../../utils/dateTimeUtils';
+import { getDayShort, getDay } from '../../../../../utils/dateTimeUtils';
 import { ReactComponent as WindCompasBg } from '../../../../../assets/img/svg/wind-compas-bg.svg';
 import { ReactComponent as WindCompas } from '../../../../../assets/img/svg/wind-compas.svg';
-import { FORECAST_SYMBOL_EXT, FORECAST_SYMBOL_LINK } from '../../../../../utils/constants';
+import {
+  FORECAST_SYMBOL_EXT,
+  FORECAST_SYMBOL_LINK,
+  DAILY_FORECAST_ALT_TEXT,
+  DAILY_FORECAST_TITLE_TEXT,
+  LOCALE_DATE
+} from '../../../../../utils/constants';
 
 function DailyForecast({ forecast }) {
   const symbolUrl = `${FORECAST_SYMBOL_LINK}${forecast.symbol}${FORECAST_SYMBOL_EXT}`;
-  const dayName = getDayShort(forecast.date, 'en-US');
+  const dayName = getDayShort(forecast.date, LOCALE_DATE);
 
   return (
     <div className={classes.forecastItem}>
       <div className={classes.title}>{dayName}</div>
-      <img className={classes.icon} src={symbolUrl} alt="forecast" title="forecast" />
+      <img
+        className={classes.icon}
+        src={symbolUrl}
+        alt={`${DAILY_FORECAST_ALT_TEXT} ${getDay(forecast.date, LOCALE_DATE)}`}
+        title={`${DAILY_FORECAST_TITLE_TEXT} ${getDay(forecast.date, LOCALE_DATE)}`}
+      />
       <div className={classes.temperature}>
         <span className={classes.sun}>{forecast.maxTemp}</span>
         <span className={classes.shadow}>{forecast.minTemp}</span>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,8 @@ export const FORECAST_SYMBOL_LINK = 'https://developer.foreca.com/static/images/
 export const FORECAST_SYMBOL_EXT = '.png';
 
 export const COOKIE_TOKEN_FIELD = 'token';
+
+export const DAILY_FORECAST_ALT_TEXT = 'Daily forecast symbol for';
+export const DAILY_FORECAST_TITLE_TEXT = 'Daily forecast symbol for';
+
+export const LOCALE_DATE = 'en-US';
