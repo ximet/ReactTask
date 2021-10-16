0932aef4336a4a28b4981b9c2425023df906faf9
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-17 00:05:41 +0300

    Adding exception handling.

--- src/components/Warnings/Warnings.jsx
+++ src/components/Warnings/Warnings.jsx
@@ -19,8 +19,14 @@ function Warnings({ currentLocation }: WarningsPropsType): React$Node {
 
   useEffect(() => {
     const getWarningsData = async (): Promise<void> => {
-      const { data } = await ApiService.getWarnings(currentLocation.id);
-      setWarnings(data.warnings);
+      try {
+        if (currentLocation.id) {
+          const { data } = await ApiService.getWarnings(currentLocation.id);
+          setWarnings(data.warnings);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     getWarningsData();
