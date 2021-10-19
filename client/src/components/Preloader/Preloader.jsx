c697a001a3c1213b562f5e830cf4f79123659eff
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-19 16:50:54 +0300

    created preloader

---
+++ client/src/components/Preloader/Preloader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './preloader.module.css'
+import sun from '../../../public/images/sun.png'
+import moon from '../../../public/images/moon.png'
+
+function Preloader() {
+    return (
+        <div className={classes.container}>
+            <img src={moon} className={classes.moon}/>
+            <img src={sun} className={classes.sunny}/>
+        </div>
+    )
+}
+
+export default Preloader
