be2ffce6ea671428288f7d333c8892ac7382edf2
Nastya <asekerich99@gmail.com>
2021-10-19 13:39:57 +0300

    refactor: remove excess props

--- src/views/WeatherForecast/WeatherForecast.jsx
+++ src/views/WeatherForecast/WeatherForecast.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import styles from './WeatherForecast.module.scss';
 
 import CityForecast from '../../layouts/CityForecast/Container';
 import DetailedForecast from '../../layouts/DetailedForecast/DetailedForecast';
 
-function WeatherForecast({ dailyCityForecast, theme }) {
+function WeatherForecast({ theme }) {
   return (
     <div className={styles.weatherForecast}>
-      <DetailedForecast
-        dailyCityForecast={dailyCityForecast}
-      />
+      <DetailedForecast />
       <CityForecast theme={theme} />
     </div>
   );
