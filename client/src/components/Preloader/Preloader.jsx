bad3d1a23a7a81aa7457ff9b615f872bd91cf56d
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-19 16:59:57 +0300

    added default objects in Preloader and ability to pass your own images

--- client/src/components/Preloader/Preloader.jsx
+++ client/src/components/Preloader/Preloader.jsx
@@ -2,14 +2,25 @@ import React from 'react';
 import classes from './preloader.module.css';
 import sun from '../../../public/images/sun.png';
 import moon from '../../../public/images/moon.png';
+import { string } from 'prop-types'
 
-function Preloader() {
+function Preloader({ centralObj, rotatingObj }) {
   return (
     <div className={classes.container}>
-      <img src={moon} className={classes.moon} />
-      <img src={sun} className={classes.sunny} />
+      <img src={centralObj} className={classes.centralObj} alt='cental obj'/>
+      <img src={rotatingObj} className={classes.rotatingObj} alt ='rotating obj'/>
     </div>
   );
+}
+
+Preloader.defaultProps = {
+    centralObj: moon,
+    rotatingObj: sun
+}
+
+Preloader.propTypes = {
+    centralObj: string,
+    rotatingObj: string
 }
 
 export default Preloader;
