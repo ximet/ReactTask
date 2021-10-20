ed3b0afc355a8ed402125993a74a26d47ae19dc5
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-20 18:28:34 +0300

    moved background colors to variables

--- client/src/components/Preloader/Preloader.jsx
+++ client/src/components/Preloader/Preloader.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import classes from './preloader.module.css';
 import sun from '../../../public/images/sun.png';
 import moon from '../../../public/images/moon.png';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-function Preloader({ staticImage, rotatingImage }) {
+function Preloader({ staticImage, rotatingImage, theme }) {
   return (
-    <div className={classes.container}>
-      <img src={staticImage} className={classes.staticImage} alt='cental obj'/>
-      <img src={rotatingImage} className={classes.rotatingImage} alt ='rotating obj'/>
+    <div className={classes.container} data-theme={theme}>
+      <img src={staticImage} className={classes.staticImage} alt="cental obj" />
+      <img src={rotatingImage} className={classes.rotatingImage} alt="rotating obj" />
     </div>
   );
 }
 
 Preloader.defaultProps = {
-    staticImage: moon,
-    rotatingImage: sun
-}
+  staticImage: moon,
+  rotatingImage: sun
+};
 
 Preloader.propTypes = {
+  theme: PropTypes.string.isRequired,
   staticImage: PropTypes.string,
   rotatingImage: PropTypes.string
-}
+};
 
 export default Preloader;
