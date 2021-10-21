044ac08546358d28f32bd436ea31bd566ad850dc
DmitriyKrivovyaz <dmitrykrivovyaz@coherentsolutions.com>
2021-10-21 14:46:13 +0300

    hardcoded images in preloader

--- client/src/components/Preloader/Preloader.jsx
+++ client/src/components/Preloader/Preloader.jsx
@@ -3,25 +3,20 @@ import classes from './preloader.module.css';
 import sun from '../../../public/images/sun.png';
 import moon from '../../../public/images/moon.png';
 import PropTypes from 'prop-types';
+import sun from '../../../public/images/sun.png';
+import moon from '../../../public/images/moon.png'
 
-function Preloader({ staticImage, rotatingImage, theme }) {
+function Preloader({ theme }) {
   return (
     <div className={classes.container} data-theme={theme}>
-      <img src={staticImage} className={classes.staticImage} alt="cental obj" />
-      <img src={rotatingImage} className={classes.rotatingImage} alt="rotating obj" />
+      <img src={moon} className={classes.staticImage} alt="cental obj" />
+      <img src={sun} className={classes.rotatingImage} alt="rotating obj" />
     </div>
   );
 }
 
-Preloader.defaultProps = {
-  staticImage: moon,
-  rotatingImage: sun
-};
-
 Preloader.propTypes = {
   theme: PropTypes.string.isRequired,
-  staticImage: PropTypes.string,
-  rotatingImage: PropTypes.string
 };
 
 export default Preloader;
