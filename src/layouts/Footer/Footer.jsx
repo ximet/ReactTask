4da89dd29bdd47818f29c6fed5b544391a6c966e
Nastya <asekerich99@gmail.com>
2021-10-25 21:12:41 +0300

    fix: add current year

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { getCurrentYear } from '../../utils/getFormattedDate';
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
     <div className={styles.footer}>
       <span>Made by J.Ell. during the React training</span>
-      <span> &copy; IsSoft Solutions, 2021</span>
+      <span> &copy; IsSoft Solutions, {getCurrentYear()}</span>
     </div>
   );
 }
