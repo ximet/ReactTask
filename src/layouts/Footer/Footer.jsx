d56e2e758017ba888e98dc72be58bd3565c73da8
Nastya <asekerich99@gmail.com>
2021-10-25 15:48:43 +0300

    feat: add footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import styles from './Footer.module.scss';
 
 function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <span>Made by J.Ell. during the React training</span>
+      <span> &copy; IsSoft Solutions, 2021</span>
+    </div>
+  );
 }
 
 export default Footer;
