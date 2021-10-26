1989cb0b62e5e40be36338c5a028bf6efb537a70
Oleksandr Viktorov <oleksandrviktorov@coherentsolutions.com>
2021-10-26 16:13:08 +0300

    [ReactTask][testing] tests for locationsSearchReducer.js done

---
+++ src/reducers/locationsSearchReducer.test.js
@@ -0,0 +1,91 @@
+import reducer from './locationsSearchReducer';
+
+describe('locations search reducer', () => {
+  const initialState = {
+    searchQuery: '',
+    searchResults: [],
+    isSearchInProgress: false
+  };
+
+  const setSearchQueryStateAction = {
+    type: 'SET_SEARCH_QUERY',
+    payload: { searchQuery: 'query example' }
+  };
+  const expectedSetSearchQueryState = {
+    searchQuery: 'query example',
+    searchResults: [],
+    isSearchInProgress: false
+  };
+
+  const setSearchInProgressAction = {
+    type: 'SET_SEARCH_IN_PROGRESS',
+    payload: { isSearchInProgress: true }
+  };
+  const expectedSetSearchInProgressState = {
+    searchQuery: '',
+    searchResults: [],
+    isSearchInProgress: true
+  };
+
+  const mockedSearchResults = [
+    {
+      id: 105128581,
+      name: 'New York City',
+      country: 'United States',
+      timezone: 'America/New_York',
+      state: 'NY',
+      adminArea: 'New York',
+      lon: -74.005973816,
+      lat: 40.714267731
+    },
+    {
+      id: 105128616,
+      name: 'New York Mills',
+      country: 'United States',
+      timezone: 'America/New_York',
+      state: 'NY',
+      adminArea: 'New York',
+      lon: -75.291275024,
+      lat: 43.10534668
+    },
+    {
+      id: 105039192,
+      name: 'New York Mills',
+      country: 'United States',
+      timezone: 'America/North_Dakota/Center',
+      state: 'MN',
+      adminArea: 'Minnesota',
+      lon: -95.376144409,
+      lat: 46.518016815
+    }
+  ];
+  const setSearchResultsAction = {
+    type: 'SET_SEARCH_RESULTS',
+    payload: { searchResults: mockedSearchResults }
+  };
+  const expectedSetSearchResultsState = {
+    searchQuery: '',
+    searchResults: mockedSearchResults,
+    isSearchInProgress: false
+  };
+
+  const clearSearchAction = { type: 'CLEAR_SEARCH' };
+
+  it('should return the initial state', () => {
+    expect(reducer(initialState, {})).toEqual(initialState);
+  });
+  it('should handle SET_SEARCH_QUERY', () => {
+    expect(reducer(initialState, setSearchQueryStateAction)).toEqual(expectedSetSearchQueryState);
+  });
+  it('should handle SET_SEARCH_IN_PROGRESS', () => {
+    expect(reducer(initialState, setSearchInProgressAction)).toEqual(
+      expectedSetSearchInProgressState
+    );
+  });
+  it('should handle SET_SEARCH_RESULTS', () => {
+    expect(reducer(initialState, setSearchResultsAction)).toEqual(expectedSetSearchResultsState);
+  });
+  it('should handle CLEAR_SEARCH', () => {
+    expect(reducer({}, clearSearchAction)).toEqual(initialState);
+  });
+});
