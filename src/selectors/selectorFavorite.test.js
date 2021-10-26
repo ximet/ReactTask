8aae35f9ae68fdb8de514b1594a30b51d837c576
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-26 15:56:40 +0300

    Creating tests.

--- src/selectors/selectorFavorite.test.js
+++ src/selectors/selectorFavorite.test.js
@@ -1,1 +1,29 @@
-describe();
+import { selectFavoriteCityIds } from './selectorsFavorite';
+
+describe('Selectors of favorite forecasts', () => {
+  it('Selector should return location ids array', () => {
+    const favoriteLocationsArray = [
+      {
+        id: 15,
+        name: 'London',
+        country: 'United Kingdom'
+      },
+      {
+        id: 20,
+        name: 'Minsk',
+        country: 'Belarus'
+      }
+    ];
+
+    const expectResult = [15, 20];
+
+    expect(selectFavoriteCityIds(favoriteLocationsArray)).toEqual(expectResult);
+  });
+
+  it('Selector should return empty array', () => {
+    const favoriteLocationsArray = [];
+    const expectResult = [];
+
+    expect(selectFavoriteCityIds(favoriteLocationsArray)).toEqual(expectResult);
+  });
+});
