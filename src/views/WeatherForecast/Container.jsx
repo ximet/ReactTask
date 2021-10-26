44d52cebb0dba3d2c9984b46caf9e78a17515f4e
Anastasiya Sekerich <65969334+JaneEllison@users.noreply.github.com>
2021-10-26 16:17:59 +0300

    Add Preloader component (#122)
    * add preloader and Error HOC
    
    * fix: merge conflicts
    
    * feat: add proptypes for components
    
    * refactor: remove excess code
    
    * refactor: change styles for spinner
    
    * feat: add proptypes for WithError component && fix: names

--- src/views/WeatherForecast/Container.jsx
+++ src/views/WeatherForecast/Container.jsx
@@ -1,34 +1,13 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
-import { FETCHING_ERROR_TEXT } from '../../constants/forecaApi';
 
 import WeatherForecast from './WeatherForecast';
-
-class WeatherForecastContainer extends React.Component {
-  render() {
-    if (this.props.isFetching) {
-      return <span> Loading...</span>;
-    }
-
-    if (this.props.error) {
-      return (
-        <div>
-          <p>{this.props.error.message}.</p>
-          <p>{FETCHING_ERROR_TEXT}</p>
-        </div>
-      );
-    }
-
-    return <WeatherForecast />;
-  }
-}
+import WithPreloader from '../../components/WithPreloader/WithPreloader';
+import WithError from '../../components/WithError/WithError';
 
 const mapStateToProps = state => ({
   isFetching: state.weather.isFetching,
-  error: state.weather.error,
-  dailyCityForecast: state.weather.dailyCityForecast,
-  cityInfo: state.location.currentLocation
+  error: state.weather.error
 });
 
-export default connect(mapStateToProps)(WeatherForecastContainer);
+export default connect(mapStateToProps)(WithPreloader(WithError(WeatherForecast)));
