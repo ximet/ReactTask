898a909ce29654f824443f318434e9ac7e43839a
Anastasiya Sekerich <65969334+JaneEllison@users.noreply.github.com>
2021-10-27 14:58:16 +0300

    feat: add view of info page (#128)

--- src/views/Info/Info.jsx
+++ src/views/Info/Info.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
+import styles from './Info.module.scss';
+import CloudImage from '../../assets/images/about-weather-cloud-img.png';
+import SunImage from '../../assets/images/about-weather-sun-img.png';
+
 function Info() {
-  return <div>Info</div>;
+  return (
+    <div className={styles.info}>
+      <div className={styles.infoItem}>
+        <div className={styles.imgContainer}>
+          <img className={styles.infoImage} src={CloudImage} alt="cloud" />
+        </div>
+        <div className={styles.infoText}>
+          <h2 className={styles.infoTitle}>Selected city forecast</h2>
+          <span className={styles.infoContent}>
+            With our app so easy to receive the weather conditions in your current location. Also
+            this website presents weather observations, weather forecasts and climatological
+            information for selected cities.
+          </span>
+        </div>
+      </div>
+      <div className={styles.infoItem}>
+        <div className={styles.infoText}>
+          <h2 className={styles.infoTitle}>Detailed forecast</h2>
+          <span className={styles.infoContent}>
+            The weather app also provides weather conditions, relative humidity, temperature in
+            different units and wind speed in addition to 7 days in future and hourly weather
+            forecast.
+          </span>
+        </div>
+        <div className={styles.imgContainer}>
+          <img className={styles.infoImage} src={SunImage} alt="sun" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
