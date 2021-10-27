d0529c5e346e969e9d48e5e9bd106505182068b6
Gleb Dronchenko <glebdronchenko@coherentsolutions.com>
2021-10-27 18:17:09 +0300

    Bug fixing.

--- src/utils/selectHelpers.js
+++ src/utils/selectHelpers.js
@@ -1,6 +1,7 @@
 import { getCurrentTime } from '../utils/dateTimeUtils';
 
-export const selectCurrentForecast = (forecasts, locationId) => forecasts[locationId]?.forecast;
+export const selectCurrentForecast = (forecasts, locationId) =>
+  forecasts[locationId]?.forecast || {};
 
 export const selectFavoriteLoadingState = (isLoadingStates, location) =>
   isLoadingStates[location?.id];
