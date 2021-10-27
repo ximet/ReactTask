ed6ffe2a23297f30d56004c8549b0a5da2b29a34
Oleksandr Viktorov <oleksandrviktorov@coherentsolutions.com>
2021-10-27 12:04:11 +0300

    [ReactTask][testing] tests for LocationsSearch component done

---
+++ src/components/LocationsSearch/LocationsSearch.test.js
@@ -0,0 +1,31 @@
+import Enzyme, { mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import LocationsSearch from './LocationsSearch';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<LocationsSearch />', () => {
+  it('should render a LocationsSearch with LocationsSearchReuslts', () => {
+    const wrapper = shallow(
+      <LocationsSearch
+        isReadyForSearch={true}
+        searchResults={[]}
+        clearSearch={() => {}}
+        searchLocations={() => {}}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render a LocationsSearch with Preloader', () => {
+    const wrapper = shallow(
+      <LocationsSearch
+        isReadyForSearch={false}
+        searchResults={[]}
+        clearSearch={() => {}}
+        searchLocations={() => {}}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
