1e58a1cfe3bf44b16d1ebabf576feafd95bf269a
Nastya <asekerich99@gmail.com>
2021-10-28 15:37:32 +0300

    feat: add tests for DetailedForecast component

---
+++ src/layouts/DetailedForecast/DetailedForecast.test.js
@@ -0,0 +1,28 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import DetailedForecast from './DetailedForecast';
+import FavoriteCities from './FavoriteCities/Container';
+import DailyForecast from './DailyForecast/Container';
+import HourlyForecast from './HourlyForecast/Container';
+
+configure({ adapter: new Adapter() });
+
+describe('DetailedForecast component', () => {
+  const renderDetailedForecast = () => shallow(<DetailedForecast />);
+
+  it('Component should contain FavoriteCities component', () => {
+    const DetailedForecast = renderDetailedForecast();
+    expect(DetailedForecast.find(FavoriteCities).exists()).toBe(true);
+  });
+
+  it('Component should contain DailyForecast component', () => {
+    const DetailedForecast = renderDetailedForecast();
+    expect(DetailedForecast.find(DailyForecast).exists()).toBe(true);
+  });
+
+  it('Component should contain DailyForecast component', () => {
+    const DetailedForecast = renderDetailedForecast();
+    expect(DetailedForecast.find(HourlyForecast).exists()).toBe(true);
+  });
+});
