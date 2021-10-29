b598a8b051dfee5fd94d2e78e9dfc44bf6bf91e6
Nastya <asekerich99@gmail.com>
2021-10-29 14:11:18 +0300

    refactor: remove magic numbers && add converting to fahrenheit function

--- src/utils/temperatureData.js
+++ src/utils/temperatureData.js
@@ -1,5 +1,13 @@
-import { UNIT_SYMBOLS } from '../constants/units';
+import {
+  UNIT_SYMBOLS,
+  RATIO_OF_ONE_CELSIUS_TO_FAHRENHEIT,
+  ZERO_CELSIUS_TO_FAHRENHEIT
+} from '../constants/units';
 
 export function getConvertedTemperature(temperature, unit) {
-  return unit === UNIT_SYMBOLS.celsius ? temperature : Math.round((temperature * 9) / 5 + 32);
+  return unit === UNIT_SYMBOLS.fahrenheit ? convertFahrenheitToCelsius(temperature) : temperature;
+}
+
+function convertFahrenheitToCelsius(temperature) {
+  return Math.round(temperature * RATIO_OF_ONE_CELSIUS_TO_FAHRENHEIT + ZERO_CELSIUS_TO_FAHRENHEIT);
 }
