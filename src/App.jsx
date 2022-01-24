63903b1dfc14cdf09ec6e27a98787723ea488dc4
Nikita Makeychik <nikitamakeychik@coherentsolutions.com>
2022-01-24 23:00:15 +0300

    got an authorization token from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from 'react'; // нужно ли импортировать в каждый файл?
+import React, { useEffect, useState } from 'react'; // нужно ли импортировать в каждый файл?
+import getToken from './getToken';
+import {  proxy, url, authData } from './constants';
 
 function App() {
-  return <div>Hello world!</div>;
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    // You need to restrict it at some point
+    // This is just dummy code and should be replaced by actual
+    if (!token) {
+      getToken(proxy, url, authData);
+    }
+  }, []);
+
+  const getToken = async (proxy, url, authData) => {
+    const res = await fetch(proxy + url + '/authorize/token', {
+      method: 'POST',
+      body: JSON.stringify(authData)
+    });
+  
+    const data = await res.json();
+  
+    setToken(data.access_token);
+  }
+
+  return <div>{token}</div>;
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const proxy = 'https://immense-hamlet-20285.herokuapp.com/';
+export const url = 'https://pfa.foreca.com';
+
+export const authData = {
+  user: 'nmakeychik1',
+  password: '64vwAHlJXbJN'
+};
