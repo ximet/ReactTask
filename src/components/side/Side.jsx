8381207d5de23b5df3643f66cb991f87e3cfae9f
Nastya Scherenkova <NastyaScherenkova@coherentsolutions.com>
2022-01-25 16:56:17 +0300

    router refactoring

--- src/components/side/Side.jsx
+++ src/components/side/Side.jsx
@@ -1,31 +1,15 @@
 import classes from './side.scss';
 
 import { Link } from 'react-router-dom';
-
-const nav = [
-  {
-    display: 'Weather',
-    path: '/'
-  },
-  {
-    display: 'World weather',
-    path: '/worldWeather'
-  },
-  {
-    display: 'Contact Us',
-    path: '/contactUs'
-  }
-];
+import { routes } from '../../config/Routes';
 
 function Side() {
   return (
     <div className={classes.side}>
       <ul className={classes.nav}>
-        {nav.map((route, index) => (
-          <li key={index}>
-            <Link to={route.path}>
-                {route.display}
-            </Link>
+        {routes.map((route) => (
+          <li key={route.path}>
+            <Link to={route.path}>{route.display}</Link>
           </li>
         ))}
       </ul>
--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -4,52 +4,33 @@ import Weather from '../pages/weather/Weather';
 import WorldWeather from '../pages/worldWeather/WorldWeather';
 import Contacts from '../pages/contacts/Contacts';
 
-const routes = [
-    {
-      display: 'Weather',
-      path: '/',
-      component: Weather,
-      isExact: true,
-    },
-    {
-      display: 'World weather',
-      path: '/worldWeather',
-      component: WorldWeather
-    },
-    {
-      display: 'Contact Us',
-      path: '/contactUs',
-      component: Contacts
-    }
+export const routes = [
+  {
+    display: 'Weather',
+    path: '/',
+    component: Weather,
+    isExact: true
+  },
+  {
+    display: 'World weather',
+    path: '/worldWeather',
+    component: WorldWeather
+  },
+  {
+    display: 'Contact Us',
+    path: '/contactUs',
+    component: Contacts
+  }
 ];
 
 function Routes() {
-    return (
-        <Switch>
-            {routes.map((route, index) => (
-                <Route key={index} 
-                    path={route.path}
-                    component={route.component}
-                    exact={route.isExact}
-                />
-            ))}
-            {/* <Route 
-                path='/'
-                exact
-                component={Weather}
-            />
-            <Route 
-                path='/worldWeather'
-                exact
-                component={WorldWeather}
-            />
-            <Route 
-                path='/contactUs'
-                exact
-                component={Contacts}
-            /> */}
-        </Switch>
-    )
+  return (
+    <Switch>
+      {routes.map((route) => (
+        <Route key={route.path} path={route.path} component={route.component} exact={route.isExact} />
+      ))}
+    </Switch>
+  );
 }
 
 export default Routes;
