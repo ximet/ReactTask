6a1b1db35e12eb064d7edb677b07e33ec9798ab4
Vitaly <vitalya_daineko@mail.ru>
2022-01-25 17:06:16 +0300

    created basic file structure

---
+++ src/components/FeedbackPage/FeedbackPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FeedbackPage() {
+  return <div className="feedback-page">Feedback page</div>;
+}
+
+export default FeedbackPage;
--- src/core/App/App.jsx
+++ src/core/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div>Hello world!!!!!!!</div>;
+  return <div className="app">Hello world!</div>;
 }
 
 export default App;
