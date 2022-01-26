3d21e0a5519b7a51a0ee9ded2324c8cd28773557
Vitaly <vitalya_daineko@mail.ru>
2022-01-26 17:34:48 +0300

    added weather api

---
+++ src/api/weatherApi.js
@@ -0,0 +1,31 @@
+const user = 'vitalya_daineko';
+const password = 'TXr1Bm3ShDik';
+const proxy = 'https://morning-cove-62020.herokuapp.com/';
+const url = `${proxy}https://pfa.foreca.com`;
+
+const WeatherApi = {
+  async getToken(expire) {
+    const currentUrl = `${url}/authorize/token?expire_hours=${expire}&user=${user}&password=${password}`;
+
+    return fetch(currentUrl)
+      .then((response) => response.json())
+      .then((data) => data.access_token);
+  },
+
+  async getLocationInfo(position, token) {
+    const currentUrl = `${url}/api/v1/location/:location?location=${position.lon},${position.lat}&token=${token}`;
+
+    return fetch(currentUrl)
+      .then((response) => response.json())
+      .then((data) => data);
+  },
+
+  async getCurrentWeather(position, token) {
+    const currentUrl = `${url}/api/v1/current/:location?location=${position.lon},${position.lat}&token=${token}`;
+    return fetch(currentUrl)
+      .then((response) => response.json())
+      .then((data) => data.current);
+  },
+};
+
+export default WeatherApi;
