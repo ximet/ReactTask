a2a51d8f48b8a87ab9a2fc67199c53d820bdc910
Vitaly <vitalya_daineko@mail.ru>
2022-01-26 18:46:50 +0300

    added getting weather data based on user location

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import weatherApi from '../../api/weatherApi';
 
 import './MainPage.scss';
 
 function MainPage() {
-  return <div className="main-page">Main page</div>;
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const [token, setToken] = useState(null);
+  const [locationInfo, setLocationInfo] = useState({});
+  const [currentWeather, setCurrentWeather] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      setCurrentPosition({ lat, lon });
+    });
+
+    weatherApi.getToken(24).then((data) => setToken(data));
+  }, []);
+
+  useEffect(() => {
+    if (currentPosition && token) {
+      weatherApi.getLocationInfo(currentPosition, token)
+        .then((data) => setLocationInfo(data));
+
+      weatherApi.getCurrentWeather(currentPosition, token)
+        .then((data) => setCurrentWeather(data));
+    }
+  }, [currentPosition, token]);
+
+  return (
+    <div className="main-page">
+      <p className="main-page__text">{`Country: ${locationInfo.country}`}</p>
+      <p className="main-page__text">{`City: ${locationInfo.name}`}</p>
+      <p className="main-page__text">{`Timezone: ${locationInfo.timezone}`}</p>
+      <p className="main-page__text">{`Time: ${currentWeather.time}`}</p>
+      <p className="main-page__text">{`Temperature: ${currentWeather.temperature}`}</p>
+    </div>
+  );
 }
 
 export default MainPage;
