63e3a5bca873013e874e6a26902cd1c9f5221731
Vitaly <vitalya_daineko@mail.ru>
2022-01-26 18:28:56 +0300

    added underlining active tab in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 import './Header.scss';
 
 function Header() {
+  const [hash, setHash] = useState(null);
+  const currentHash = useLocation().pathname;
+
+  useEffect(() => {
+    setHash(currentHash);
+  }, [currentHash]);
+
   return (
     <div className="header">
       <Link className="header__logo" to="/">WeatherApp</Link>
       <nav className="menu">
         <ul className="menu__list">
           <li className="menu__list-item">
-            <Link className="menu__list-link" to="/">Home</Link>
+            <Link
+              className={`menu__list-link ${hash === '/' ? 'active' : ''}`}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li className="menu__list-item">
-            <Link className="menu__list-link" to="/info">Info</Link>
+            <Link
+              className={`menu__list-link ${hash === '/info' ? 'active' : ''}`}
+              to="/info"
+            >
+              Info
+            </Link>
           </li>
           <li className="menu__list-item">
-            <Link className="menu__list-link" to="/feedback">Feedback</Link>
+            <Link
+              className={`menu__list-link ${hash === '/feedback' ? 'active' : ''}`}
+              to="/feedback"
+            >
+              Feedback
+            </Link>
           </li>
         </ul>
       </nav>
