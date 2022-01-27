321162523ecfb05e688fd0673a378f5430b2233d
Nastya <shchernast00@mail.ru>
2022-01-27 13:58:14 +0300

    refactoring

--- src/components/side/Side.jsx
+++ src/components/side/Side.jsx
@@ -1,31 +1,15 @@
 import classes from './side.scss';
 
-import { Link } from 'react-router-dom';
-
-const nav = [
-  {
-    display: 'Weather',
-    path: '/'
-  },
-  {
-    display: 'World weather',
-    path: '/worldWeather'
-  },
-  {
-    display: 'Feedback',
-    path: '/feedback',
-  }
-];
+import { NavLink } from 'react-router-dom';
+import { routes } from '../../config/Routes';
 
 function Side() {
   return (
     <div className={classes.side}>
       <ul className={classes.nav}>
-        {nav.map((route, index) => (
-          <li key={index}>
-            <Link to={route.path}>
-                {route.display}
-            </Link>
+        {routes.map((route) => (
+          <li key={route.path}>
+            <NavLink to={route.path} exact={route.isExact} activeClassName={classes.active}>{route.display}</NavLink>
           </li>
         ))}
       </ul>
--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -4,37 +4,33 @@ import WeatherPage from '../pages/weatherPage/WeatherPage';
 import WorldWeatherPage from '../pages/worldWeatherPage/WorldWeatherPage';
 import FeedbackPage from '../pages/feedbackPage/FeedbackPage';
 
-const routes = [
-    {
-      display: 'Weather',
-      path: '/',
-      component: WeatherPage,
-      isExact: true,
-    },
-    {
-      display: 'World weather',
-      path: '/worldWeather',
-      component: WorldWeatherPage
-    },
-    {
-      display: 'Feedback',
-      path: '/feedback',
-      component: FeedbackPage
-    }
+export const routes = [
+  {
+    display: 'Weather',
+    path: '/',
+    component: WeatherPage,
+    isExact: true
+  },
+  {
+    display: 'World weather',
+    path: '/worldWeather',
+    component: WorldWeatherPage
+  },
+  {
+    display: 'Feedback',
+    path: '/feedback',
+    component: FeedbackPage
+  }
 ];
 
 function Routes() {
-    return (
-        <Switch>
-            {routes.map((route, index) => (
-                <Route key={index} 
-                    path={route.path}
-                    component={route.component}
-                    exact={route.isExact}
-                />
-            ))}    
-        </Switch>
-    )
+  return (
+    <Switch>
+      {routes.map((route) => (
+        <Route key={route.path} path={route.path} component={route.component} exact={route.isExact} />
+      ))}
+    </Switch>
+  );
 }
 
 export default Routes;
