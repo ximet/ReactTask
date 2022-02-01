4b3fe10c464524ce9bdbba68d9fe80465b3b24e8
MariaMalakhova <84703112+MariaMalakhovaCSI@users.noreply.github.com>
2022-02-01 11:32:31 +0300

    Basic file structure (#163)
    * Init project
    
    * File structure and views

---
+++ src/views/Info/Info.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from '../../App.module.css';
+
+function Info() {
+  return (
+    <div className={classes.image_container}>
+      <img alt="" />
+      <h1 className={classes.info}>Info</h1>
+    </div>
+  );
+}
+
+export default Info;
