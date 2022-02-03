a8d358f30260a556967935aaeb32f4dfab2bf08a
Nastya <shchernast00@mail.ru>
2022-02-03 21:50:48 +0300

    add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import classes from './footer.scss';
+
+function Footer() {
+  return (
+    <div className={classes.footer}>
+      <div>Footer</div>
+    </div>
+  );
+}
+
+export default Footer;
