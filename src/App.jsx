8ab0e750cabfa5812564f28a8b3afc66c67dec10
Maria Malakhova <mariamalakhova@coherentsolutions.com>
2022-02-07 18:45:41 +0300

    Weather api setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import classes from './App.module.css';
 import NavBar from './components/NavBar/NavBar';
@@ -6,13 +6,22 @@ import Footer from './components/Footer/Footer';
 import Home from './views/Home/Home';
 import Info from './views/Info/Info';
 import Feedback from './views/Feedback/Feedback';
+import weatherApi from './services/WeatherApi';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    weatherApi.getToken().then(accessToken => setToken(accessToken));
+  }, []);
+
   return (
     <Router>
       <NavBar />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route exact path="/">
+          <Home token={token} />
+        </Route>
         <Route path="/info" component={Info} />
         <Route path="/feedback" component={Feedback} />
       </Switch>
