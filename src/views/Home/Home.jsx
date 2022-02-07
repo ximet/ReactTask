a9fd597734b18b3b6bc151cd0e875f92ec7a4fc2
Maria Malakhova <mariamalakhova@coherentsolutions.com>
2022-02-07 19:29:41 +0300

    Current weather info on Home view

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 import classes from '../../App.module.css';
-import Card from '../../atomic-components/Card/Card';
+import weatherApi from '../../services/WeatherApi';
+import CurrentWeather from '../../components/CurrentWeather/CurrentWeather';
 
-function Home() {
+function Home({ token }) {
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const [locationInfo, setLocationInfo] = useState({});
+  const [currentWeather, setCurrentWeather] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      setCurrentPosition({ lat, lon });
+    });
+  }, []);
+
+  useEffect(() => {
+    if (currentPosition && token) {
+      weatherApi.getLocationInfo(currentPosition, token).then(data => setLocationInfo(data));
+      weatherApi.getCurrentWeather(currentPosition, token).then(data => setCurrentWeather(data));
+    }
+  }, [currentPosition, token]);
+
+  const currentDate = moment(currentWeather.time).format('dddd, Do MMMM');
+
   return (
     <div className="mainSection">
       <div className={classes.image_container}>
-        <Card />
+        <CurrentWeather
+          locationInfo={locationInfo}
+          currentWeather={currentWeather}
+          currentDate={currentDate}
+        />
       </div>
     </div>
   );
