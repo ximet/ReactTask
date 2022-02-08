090256f186548c94c207e8eab2544a39d9eb3560
Vitaly <vitalya_daineko@mail.ru>
2022-02-08 16:15:14 +0300

    fixed error 429 from server

--- src/components/WorldWeatherPage/WorldWeatherPage.jsx
+++ src/components/WorldWeatherPage/WorldWeatherPage.jsx
@@ -13,8 +13,8 @@ const locationDisplayedCities = [
   '37.618423,55.751244', /* Moscow */
   '13.404954,52.520008', /* Berlin */
   '121.469170,31.224361', /* Shanghai */
-  /* '116.383331,39.916668', Beijing
-  '72.877426,19.076090', Mumbai
+  '116.383331,39.916668', /* Beijing */
+  /* '72.877426,19.076090', Mumbai
   '28.979530,41.015137', Istanbul
   '139.839478,35.652832', Tokyo
   '-73.935242,40.730610', New York
@@ -24,18 +24,21 @@ const locationDisplayedCities = [
 function WorldWeatherPage() {
   const [worldWeather, setWorldWeather] = useContext(WorldWeatherContext);
 
+  // using setTimeout to bypass 429 error from server (Too Many Requests);
   useEffect(() => {
     if (!worldWeather.length) {
       const token = getCookie('token');
-      locationDisplayedCities.forEach((cityLocation) => {
-        weatherApi.getLocationInfo(cityLocation, token)
-          .then((data) => {
-            weatherApi.getCurrentWeather(cityLocation, token)
-              .then((weather) => {
-                const fullData = { ...data, ...weather };
-                setWorldWeather((prev) => ([...prev, fullData]));
-              });
-          });
+      locationDisplayedCities.forEach((cityLocation, index) => {
+        setTimeout(() => {
+          weatherApi.getLocationInfo(cityLocation, token)
+            .then((data) => {
+              weatherApi.getCurrentWeather(cityLocation, token)
+                .then((weather) => {
+                  const fullData = { ...data, ...weather };
+                  setWorldWeather((prev) => ([...prev, fullData]));
+                });
+            });
+        }, (index + 1) * 250);
       });
     }
   }, []);
