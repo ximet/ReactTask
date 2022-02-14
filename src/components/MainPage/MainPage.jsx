4db682f033674bf1ca120b4c7af427d2bca80ee3
Vitaly <vitalya_daineko@mail.ru>
2022-02-14 17:47:08 +0300

    added processing and output of server errors

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,46 +1,47 @@
 import React, { useEffect, useState } from 'react';
 
 import weatherApi from '../../api/weatherApi';
+import useRequest from '../../hooks/useRequest';
 import SelectedCityInfo from '../SelectedCityInfo/SelectedCityInfo';
 import Preloader from '../Preloader/Preloader';
+import Error from '../Error/Error';
 
 import './MainPage.scss';
 
 function MainPage() {
-  const [currentPosition, setCurrentPosition] = useState(null);
-  const [locationInfo, setLocationInfo] = useState(null);
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [todaysWeather, setTodaysWeather] = useState(null);
-
-  const [nextWeekWeather, setNextWeekWeather] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [locationInfo, locationInfoError] = useRequest(weatherApi.getLocationInfo, location);
+  const [currentWeather, currentWeatherError] = useRequest(weatherApi.getCurrentWeather, location);
+  const [todaysWeather, todaysWeatherError] = useRequest(weatherApi.getTodaysWeather, location);
+  const [dailyWeather, dailyWeatherError] = useRequest(weatherApi.getNextWeekWeather, location);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       const currentPos = `${position.coords.longitude},${position.coords.latitude}`;
-      setCurrentPosition(currentPos);
+      setLocation(currentPos);
     });
   }, []);
 
-  useEffect(() => {
-    if (currentPosition) {
-      weatherApi.getLocationInfo(currentPosition).then(setLocationInfo);
-      weatherApi.getCurrentWeather(currentPosition).then(setCurrentWeather);
-      weatherApi.getTodaysWeather(currentPosition).then(setTodaysWeather);
-      weatherApi.getNextWeekWeather(currentPosition).then(setNextWeekWeather);
-    }
-  }, [currentPosition]);
-
   return (
-    (locationInfo && currentWeather && todaysWeather && nextWeekWeather)
+    (locationInfo && currentWeather && todaysWeather && dailyWeather)
       ? (
         <SelectedCityInfo
           locationInfo={locationInfo}
           currentWeather={currentWeather}
           todaysWeather={todaysWeather}
-          nextWeekWeather={nextWeekWeather}
+          nextWeekWeather={dailyWeather}
         />
       )
-      : <Preloader />
+      : (locationInfoError || currentWeatherError || todaysWeatherError || dailyWeatherError)
+        ? (
+          <Error errors={[
+            locationInfoError,
+            currentWeatherError,
+            todaysWeatherError,
+            dailyWeatherError]}
+          />
+        )
+        : <Preloader />
   );
 }
 
