fbab977fdca2fbfa42b453ddded09e1947059f1a
Nikita Makeychik <nikitamakeychik@coherentsolutions.com>
2022-02-15 00:27:33 +0300

    moved logic to appropriate component, added some general app styles

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,10 @@ import MainPage from './views/MainPage/MainPage';
 
 function App() {
   const [token, setToken] = useState('');
-  const [coords, setCoords] = useState({});
-  const [searchedLocation, setSearchedLocation] = useState({});
-
-  const isSearchedLocationEmpty = Object.keys(searchedLocation).length === 0;
-
-  const updateSearchedLocation = location => {
-    setSearchedLocation(location);
-  };
 
   useEffect(() => {
     getToken(url).then(token => setToken(token));
-    navigator.geolocation.getCurrentPosition(
-      pos => {
-        setCoords(pos.coords);
-      },
-      err => {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-      }
-    );
+    // set token to cookies
   }, []);
 
   return (
@@ -41,32 +26,16 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/">
-            <MainPage>
-              <SearchLocation token={token} updateSearchedLocation={updateSearchedLocation} />
-              {isSearchedLocationEmpty ? (
-                ''
-              ) : (
-                <CurrentWeather
-                  title={'Current weather from searched location'}
-                  token={token}
-                  locationId={searchedLocation.id}
-                />
-              )}
-              <CurrentWeather
-                title={'Current weather from your location'}
-                token={token}
-                coords={coords}
-              />
-            </MainPage>
+            <MainPage token={token} />
           </Route>
           <Route path="/weather">
-            <ListPage />
+            <ListPage token={token} />
           </Route>
           <Route path="/about">
-            <InfoPage />
+            <InfoPage token={token} />
           </Route>
           <Route path="/feedback">
-            <FeedbackPage />
+            <FeedbackPage token={token} />
           </Route>
         </Switch>
         <Footer />
