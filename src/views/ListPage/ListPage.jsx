fd9ca1384337f63305f2016461cfc1b0e59d3bf1
Nikita Makeychik <nikitamakeychik@coherentsolutions.com>
2022-02-16 17:33:38 +0300

    implemented rendering list of weather for several locations

--- src/views/ListPage/ListPage.jsx
+++ src/views/ListPage/ListPage.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCurrentWeatherById, getLocationInfoByName } from '../../api';
+import LocationHeader from '../../components/LocationHeader/LocationHeader';
+import WeatherCard from '../../components/WeatherCard/WeatherCard';
+import { url } from '../../constants';
 import commonClasses from '../common.scss';
 
-const ListPage = () => {
-  const isSearchedLocationEmpty = Object.keys(searchedLocation).length === 0;
+const ListPage = ({ token }) => {
+  const locationNames = [
+    'London',
+    'Tokyo',
+    'Paris',
+    'Rome',
+    // 'Washington D.C.',
+    'Berlin',
+    'New York'
+  ];
 
-  const [searchedLocation, setSearchedLocation] = useState({});
+  const [weatherCards, setWeatherCards] = useState([]);
+  // const [weatherList, setWeatherList] = useState([]);
 
-  const updateSearchedLocation = location => {
-    setSearchedLocation(location);
-  };
+  useEffect(() => {
+    locationNames.forEach(async name => {
+      const location = await getLocationInfoByName(url, token, name);
+      const weather = await getCurrentWeatherById(url, token, location.id);
 
-  return (
-    <main className={commonClasses.page}>
-      <SearchLocation token={token} updateSearchedLocation={updateSearchedLocation} />
-      {isSearchedLocationEmpty ? (
-        ''
-      ) : (
-        <CurrentWeather
-          title={'Current weather from searched location'}
-          token={token}
-          locationId={searchedLocation.id}
-        />
-      )}
-    </main>
-  );
+      const element = (
+        <div key={location.id}>
+          <LocationHeader name={location.name} country={location.country} />
+          <WeatherCard weather={weather} />
+        </div>
+      );
+
+      setWeatherCards(prev => [...prev, element]);
+    });
+    // setLocations(locations);
+    // get list of weather
+    // const weatherList = locations.map(location => getCurrentWeatherById(url, token, location.id));
+    // setWeatherList(weatherList);
+  }, []);
+
+  return <main className={commonClasses.page}>{weatherCards}</main>;
 };
 
 export default ListPage;
