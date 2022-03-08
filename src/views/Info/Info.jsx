f84517a5f84f21d9aa80e7b0409e30f4ffa6b120
MariaMalakhova <84703112+MariaMalakhovaCSI@users.noreply.github.com>
2022-03-08 15:13:34 +0300

    Info view (#182)
    * Init project
    
    * File structure and views
    
    * Info view

--- src/views/Info/Info.jsx
+++ src/views/Info/Info.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import classes from '../../App.module.css';
+import classes from './Info.module.css';
 
 function Info() {
   return (
-    <div className={classes.image_container}>
-      <img alt="" />
-      <h1 className={classes.info}>Info</h1>
+    <div className={classes.info_wrapper}>
+      <div className={classes.image_container}>
+        <div className={classes.info_item}>
+          <h2 className={classes.info_title}>About Us</h2>
+          <span className={classes.info_subtitle}>
+            This website presents weather observations according to{' '}
+            <a
+              href="https://developer.foreca.com/"
+              target="_blank"
+              className={classes.foreca_link}
+              rel="noreferrer"
+            >
+              Foreca API
+            </a>
+            , weather forecasts and climatological information for selected cities.
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
