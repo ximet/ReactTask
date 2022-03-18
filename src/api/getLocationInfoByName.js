6582eb6b41b6ab5b3f0f899a75160aa0260f11af
Nikita Makeychik <mikitamakeichyk@gmail.com>
2022-03-19 01:21:46 +0400

    renamed and edit api method

--- src/api/getLocationInfoByName.js
+++
@@ -1,9 +0,0 @@
-const getLocationInfoByName = async (url, locationName) => {
-  const res = await fetch(url + `/api/v1/location/search/${locationName}`);
-
-  const data = await res.json();
-
-  return data.locations[0];
-};
-
-export default getLocationInfoByName;
