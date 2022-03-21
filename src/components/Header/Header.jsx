00b83a0aa6d36f58a57e7e7574aa90c1b7939dd5
Nikita Makeychik <mikitamakeichyk@gmail.com>
2022-03-21 12:03:58 +0400

    layout, fonts, scss variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import SearchInput from '../SearchInput/SearchInput';
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
 import classes from './Header.module.scss';
 
 const Header = () => {
   return (
     <header className={classes.header}>
       <div className={classes.header__container}>
-        <h1 className={classes.logo}>Weather</h1>
+        <ThemeSwitcher />
+        <h1 className={classes.logo}>minWEATHER</h1>
         <Navbar />
         <SearchInput />
       </div>
