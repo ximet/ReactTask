394c5a1de69609cbe3876fa24452fe3d898d4cda
Maria Malakhova <mariamalakhova@coherentsolutions.com>
2022-03-24 22:08:18 +0300

    Feedback form validation

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -34,32 +34,54 @@ function FeedbackForm() {
         type="text"
         placeholder="First name"
         className={classes.input_text}
-        {...register('firstName', { required: true, maxLength: 20 })}
+        {...register('firstName', {
+          required: 'First name is required',
+          maxLength: {
+            value: 20,
+            message: 'This input exceed maxLength'
+          }
+        })}
       />
+      {errors.firstName && <p className={classes.error}>{errors.firstName.message}</p>}
 
       <input
         type="text"
         placeholder="Last name"
         className={classes.input_text}
-        {...register('lastName', { required: true, maxLength: 30 })}
+        {...register('lastName', {
+          required: 'Last name is required',
+          maxLength: {
+            value: 20,
+            message: 'This input exceed maxLength'
+          }
+        })}
       />
+      {errors.lastName && <p className={classes.error}>{errors.lastName.message}</p>}
 
       <input
         type="text"
         placeholder="Email"
         className={classes.input_text}
-        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        {...register('email', {
+          required: 'Email is required',
+          pattern: {
+            value: /\S+@\S+\.\S+/i,
+            message: 'Entered value does not match email format'
+          }
+        })}
       />
+      {errors.email && <p className={classes.error}>{errors.email.message}</p>}
 
       <p className={classes.feedback_subtitle}>How would you rate the App?</p>
       <Controller
         control={control}
         name="rating"
-        rules={{ required: true }}
+        rules={{ required: 'Please rate our App' }}
         render={({ field: { onChange, value } }) => (
           <StarRating setRating={onChange} value={value} />
         )}
       />
+      {errors.rating && <p className={classes.error}>{errors.rating.message}</p>}
 
       <div className={classes.textarea_wrapper}>
         <p className={classes.feedback_subtitle}>
@@ -67,15 +89,20 @@ function FeedbackForm() {
             ? 'What was disappointing in your experience with us?'
             : 'What features do you value the most?'}
         </p>
-        <textarea className={classes.textarea_input} {...register('feedback')} />
+        <textarea className={classes.textarea_input} {...register('feedback', {
+          maxLength: {
+            value: 255,
+            message: 'This input exceed maxLength'
+          }
+        })} />
+        {errors.feedback && <p className={classes.error}>{errors.feedback.message}</p>}
       </div>
 
-      {errors.firstName && <p className={classes.input_error}>First name field should be filled</p>}
-      {errors.lastName && <p className={classes.input_error}>Last name field should be filled</p>}
-      {errors.email && <p className={classes.input_error}>Please enter email</p>}
-      {errors.rating && <p className={classes.input_error}>Please rate our App</p>}
-
-      <input type="submit" value="Send feedback" className={classes.input_submit} />
+      <input
+        type="submit"
+        value="Send feedback"
+        className={classes.input_submit}
+      />
     </form>
   );
 }
