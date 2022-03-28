07aabe3e72d077b46850ec9f30cb9e1c565f3db8
MariaMalakhova <84703112+MariaMalakhovaCSI@users.noreply.github.com>
2022-03-28 18:38:54 +0300

    Adjust feedback page (#195)
    * Init project
    
    * File structure and views
    
    * Generic background for all views
    
    * Feedback form validation
    
    * Star rating to default onSubmit
    
    * Toast on Feedback form submit
    
    * Remove commented code, specify element styles

--- src/views/Feedback/Feedback.jsx
+++ src/views/Feedback/Feedback.jsx
@@ -5,12 +5,12 @@ import FeedbackForm from '../../components/FeedbackForm/FeedbackForm';
 function Feedback() {
   return (
     <div className={classes.feedback}>
-      <div className={classes.image_container} alt="background">
+      <div className={classes.feedback_wrapper}>
         <div className={classes.form__wrapper}>
           <h2 className={classes.feedback_title}>Feedback Form</h2>
-          <span className={classes.feedback_subtitle}>
+          <p className={classes.feedback_subtitle}>
             What do you think about our app? Rate your experience
-          </span>
+          </p>
           <FeedbackForm />
         </div>
       </div>
