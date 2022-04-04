2e7726b0e25c9617a5b8ca0a59bca2db62e77658
MariaMalakhova <84703112+MariaMalakhovaCSI@users.noreply.github.com>
2022-04-04 17:29:49 +0300

    Adjust search functionality (#196)
    * Init project
    
    * File structure and views
    
    * Remove mocked Daily forecast
    
    * Search input as form
    
    * Dynamic search and Home view update accordingly
    
    * Add ThemeContext
    
    * ThemeSwitcher context

---
+++ src/atomic-components/LocationTile/LocationTile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const formatCityTile = city => {
+  if (!city.country) {
+    return `${city.name}`;
+  }
+  const formattedTile = `${city.name}, ${city.country}`;
+  if (city.state) {
+    return formattedTile.concat(`, ${city.state}`);
+  }
+  return formattedTile;
+};
+
+const LocationTile = ({ location, selected }) => {
+  const history = useHistory();
+
+  const handleMouseDown = () => {
+    history.push(`/location/${location.id}`);
+    selected();
+  };
+
+  return <div onMouseDown={handleMouseDown}>{formatCityTile(location)}</div>;
+};
+
+export default LocationTile;
