f264d999d5f61b93dc29e55db97da7d9cec709c5
MariaMalakhova <84703112+MariaMalakhovaCSI@users.noreply.github.com>
2022-04-06 20:22:38 +0300

    Add Loader on Home view (#197)
    * Init project
    
    * File structure and views
    
    * Add Loader on Home screen
    
    * Loader after async

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,15 +4,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { weatherApi } from '../../services/WeatherApi';
 import CurrentLocationForecast from '../../components/CurrentLocationForecast/CurrentLocationForecast';
+import Loader from '../../atomic-components/Loader/Loader';
 import { setCurrentLocation } from '../../redux/actions/locationActions';
 import { locationSelector } from '../../redux/selectors/locationSelector';
 import classes from './Home.module.css';
 
 function Home({ token }) {
   const dispatch = useDispatch();
-  const { location } = useSelector(locationSelector);
   const params = useParams();
-
+  const { location } = useSelector(locationSelector);
+  const [isLoading, setLoading] = useState(true);
   const [locationInfo, setLocationInfo] = useState({});
   const [currentWeather, setCurrentWeather] = useState({});
   const [dailyForecast, setDailyForecast] = useState([]);
@@ -34,14 +35,19 @@ function Home({ token }) {
   useEffect(() => {
     if (token && location) {
       weatherApi.getLocationInfo(location, token).then(data => setLocationInfo(data));
-      weatherApi.getCurrentWeather(location, token).then(data => setCurrentWeather(data));
+      weatherApi.getCurrentWeather(location, token).then(data => {
+        setCurrentWeather(data);
+        setLoading(false);
+      });
       weatherApi.getDailyForecast(location, token).then(data => setDailyForecast(data));
     }
   }, [location, token]);
 
   const currentDate = moment(currentWeather.time).format('dddd, Do MMMM');
 
-  return (
+  return isLoading ? (
+    <Loader />
+  ) : (
     <div className={classes.home_view_wrapper}>
       <CurrentLocationForecast
         locationInfo={locationInfo}
