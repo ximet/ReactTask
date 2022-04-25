63ff0045afbeb291551741516f3d755d588108ba
mrvanagas <lukonelis1@gmail.com>
2022-04-25 10:17:20 +0300

    bolierplate
    boilerplate commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+// const token = process.env.REACT_APP_TOKEN;
+// console.log(token);
+
 function App() {
-  return <div>Hello world!</div>;
+  const test = () => {
+    const token = ''
+    // const weatherAPIKey = process.env.REACT_APP_WEATHER_API_KEY;
+    fetch('https://pfa.foreca.com/api/v1/forecast/daily/100598316', {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  };
+  return (
+    <div>
+      <button onClick={test}>Click me</button>
+    </div>
+  );
 }
 
 export default App;
