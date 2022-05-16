947b28f2bf6c8a63104ee8165af650de1d6e87af
mrvanagas <lukonelis1@gmail.com>
2022-05-16 10:40:22 +0300

    feat: added date formating
    Added dateHelper file, to format the full date into weekday.

--- client/src/Components/MultiCard.jsx
+++ client/src/Components/MultiCard.jsx
@@ -7,7 +7,9 @@ const MultiWeather = ({ data }) => {
       {data.map((info, index) => (
         <Style.CardContainer key={index}>
           <section>
-            <Style.Description>Low:{info.minTemp}C High:{info.maxTemp} C</Style.Description>
+            <Style.Description>
+              Low:{info.minTemp}C High:{info.maxTemp} C
+            </Style.Description>
             <Style.Description>{info.maxWindSpeed} m/s</Style.Description>
             <Style.Description>{dateHelper(info.date)}</Style.Description>
           </section>
--- client/src/Utils/dateHelper.js
+++ client/src/Utils/dateHelper.js
@@ -1,17 +1,17 @@
 function dateHelper(date) {
-    const weekDay = new Date(date).getDay();
+  const weekDay = new Date(date).getDay();
 
-    const weekDayMap = {
+  const weekDayMap = {
     0: 'Monday',
     1: 'Tuesday',
-    2: "Wednesday",
+    2: 'Wednesday',
     3: 'Thursday',
-    4: "Friday",
-    5: "Saturday",
-    6: "Sunday"
-    }
+    4: 'Friday',
+    5: 'Saturday',
+    6: 'Sunday'
+  };
 
-    return weekDayMap[weekDay]
+  return weekDayMap[weekDay];
 }
 
 export default dateHelper;
