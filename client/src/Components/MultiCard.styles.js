3c552c1f8234e1f61b88b9935e07bfd09de0fef5
mrvanagas <lukonelis1@gmail.com>
2022-05-20 15:41:48 +0300

    feat: added first iteration of debouncing
    Added first iteration of debouncing the search in the city search page.

--- client/src/Components/MultiCard.styles.js
+++ client/src/Components/MultiCard.styles.js
@@ -1,27 +1,25 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
-    align-content: center;
-    margin: 1rem;
-    padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  margin: 1rem;
+  padding: 1rem;
 `;
 
 export const CardContainer = styled.div`
   background-color: gray;
   border-radius: 0.5rem;
-    padding: 1rem;
-    margin: 1rem;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-
+  padding: 1rem;
+  margin: 1rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 export const Description = styled.div`
--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import * as Style from './Searchbar.styles';
 import { weatherAPI } from '../API/api';
 import endpoints from '../Utils/endpoints';
@@ -6,23 +6,43 @@ import { minCharacters } from '../Utils/minChars';
 import MultiWeather from './MultiCard';
 
 export default function SearchBar() {
-  const [cityName, setCityName] = useState('');
+  const [inputValue, setInputValue] = useState('');
   const [results, setResults] = useState([]);
   const [chooseCityForecast, setChooseCityForecast] = useState([]);
+  const [count, setCount] = useState(10)
+ 
 
   useEffect(() => {
-    if (cityName.length > minCharacters) {
-      loadCities();
-    }
-  }, [cityName, loadCities]);
+    let timerID
+
+    if (inputValue.length > minCharacters) {
+      console.log('start of request')
+     timerID = setTimeout(async () =>{
+        try {
+          console.log('request started')
+          const res = await weatherAPI.get(endpoints.GET_CITY(inputValue));
+          setResults(res.data.locations);
+        } catch (error) {
+          alert(error);
+        }
+      }
+    , 1500)}
+    return () => {
+      console.log(timerID)
+      clearTimeout(timerID)
+    } 
+  }, [inputValue]);
+
+  
 
   const cityValueHandler = value => {
-    setCityName(value);
+    setInputValue(value);
   };
 
   const loadCities = async () => {
+    console.log(inputValue)
     try {
-      const res = await weatherAPI.get(endpoints.GET_CITY(cityName));
+      const res = await weatherAPI.get(endpoints.GET_CITY(inputValue));
       setResults(res.data.locations);
     } catch (error) {
       alert(error);
@@ -34,12 +54,13 @@ export default function SearchBar() {
       const res = await weatherAPI.get(endpoints.GET_DAILY_BY_ID(city.id));
       console.log(res);
       setChooseCityForecast(res.data.forecast);
-      setCityName('');
+      setInputValue('');
       setResults([]);
     } catch (error) {
       alert(error);
     }
   };
+
   return (
     <Style.Container>
       <h1>Search by City</h1>
@@ -48,12 +69,12 @@ export default function SearchBar() {
         <Style.SearchInner>
           <Style.Input
             type="text"
-            value={cityName}
+            value={inputValue}
             onChange={e => cityValueHandler(e.target.value)}
           />
         </Style.SearchInner>
         <Style.Dropdown>
-          {cityName.length > minCharacters ? (
+          {inputValue.length > minCharacters ? (
             <Style.DropdownRow results={results}>
               {results.map(result => (
                 <div key={result.id}>
@@ -71,6 +92,14 @@ export default function SearchBar() {
           <MultiWeather data={chooseCityForecast} />
         </section>
       )}
+
+      <div>
+        <div>{count}</div>
+        <button onClick={() => setCount(c => c +1)}>Count</button>
+      </div>
     </Style.Container>
+
+
+        
   );
 }
