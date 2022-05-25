440ec4f68e9af43618da073807569ac7d37a04cf
Lukas Vanagas <71028004+mrvanagas@users.noreply.github.com>
2022-05-25 16:53:01 +0300

    Redux implementation (#219)
    * basic api functions
    
    Added 3 basic API functions, also gets local coordinates
    
    * basic API functions
    
    Basic API functions with 3 calls
    
    * Project restructure, added server
    
    Restructured project structure to separate folders, added backend server to generate tokens.
    
    * removed token
    
    Removed hardcoded token
    
    * Adjusted geolocator function
    
    Geolocator now is called automatically in API.js file to get current current coordinates
    
    * Auto weather info
    
    Added logic to automatically show weather, based on current position, using the geolocator
    
    * code modification
    
    * Removed hardcoded token, added other pages and router
    
    Removed hard coded token, added several pages as placeholders, added router
    
    * Removed token
    
    Removed hardcoded token
    
    * added 1st iteration of silent auth
    
    Added first iteration of silent authentication, exploring other options with timed tokens
    
    * fixed files
    
    fixed wrong file deletion
    
    * first iteration of search
    
    First iteration of search functionality, first working with static data, in order to make the foundation of the search, will change to getting the info from API
    
    * adjustments
    
    * Added styled components
    
    Refactored code to use styled components instead of regular css
    
    * added search from API
    
    Added search from API, now need to figure out how to pass city id to the end point
    
    * removed unneeded file
    
    * Added city search
    
    Added city search by entering part of the city name.
    
    * Added dateHelper
    
    Added datehelper, to convert date to weekdays, shortened styles.
    
    * feat: added date formating
    
    Added dateHelper file, to format the full date into weekday.
    
    * feat: added first iteration of debouncing
    
    Added first iteration of debouncing the search in the city search page.
    
    * Feat: added contact page
    
    Added contact page. Validation and form handling is done by Formik. When submitting fields, the info is set into localStorage
    
    * Added Comment title
    
    * added more conditions for input
    
    * Code refactor
    
    Destructured variables, refactored some code, to make it shorter and more readable.
    
    * Ran formatter
    
    * feat: redux Implementation
    
    Remade branch for Redux implementation
    
    * redux implementation
    
    Implemented redux
    
    * Ran formatter, removed unneeded file
    
    Co-authored-by: lukasvancs <lukasvanagas@coherentsolutions.com>

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import store from './store/index.js';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
---
+++ client/src/store/actionType.js
@@ -0,0 +1,1 @@
+export const CITY_SELECT = 'CITY_SELECT';
