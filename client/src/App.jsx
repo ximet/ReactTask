704745af5faba9e19324e77670890fa99fbb52b0
mrvanagas <lukonelis1@gmail.com>
2022-05-25 15:09:32 +0300

    feat: redux Implementation
    Remade branch for Redux implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom';
 
 import Home from './Pages/Home';
