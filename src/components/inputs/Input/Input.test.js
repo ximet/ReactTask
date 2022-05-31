e59d0efd5d7e3a8fc5ccae0320a6db5beb0a1432
Gedas Incirauskas <gedasincirauskas@coherentsolutions.com>
2022-05-31 16:22:05 +0300

    added test for input component

---
+++ src/components/inputs/Input/Input.test.js
@@ -0,0 +1,19 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Input from './Input';
+
+describe('When user enter some text in the input', () => {
+  test('should display the text in the input', () => {
+    render(<Input />);
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: 'Test' } });
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  test('should display an error', () => {
+    render(<Input isError={true} errorMessage={'Error message'} />);
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: '' } });
+    fireEvent.blur(screen.getByRole('textbox'));
+    screen.debug();
+    expect(screen.getByText(/Error message/)).toBeInTheDocument();
+  });
+});
