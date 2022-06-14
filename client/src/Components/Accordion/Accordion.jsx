c965384ff97e09df8169bbb1aad6ab122bde5652
mrvanagas <lukonelis1@gmail.com>
2022-06-14 16:12:47 +0300

    First iteration of accordion

--- client/src/Components/Accordion/Accordion.jsx
+++ client/src/Components/Accordion/Accordion.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Style from './Accordion.styles';
+import { Data } from './accordionData';
 
 const Accordion = () => {
-  return <div>Accordion</div>;
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = index => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
+  return (
+    <Style.AccordionSection>
+      <Style.Container>
+        {Data.map((item, index) => {
+          return (
+            <>
+              <Style.Wrap onClick={() => toggle(index)} key={index}>
+                <h1>{item.question}</h1>
+                <span>{clicked === index ? '+' : '-'}</span>
+              </Style.Wrap>
+              {clicked === index ? (
+                <Style.Dropdown>
+                  <p>{item.answer}</p>
+                </Style.Dropdown>
+              ) : null}
+            </>
+          );
+        })}
+      </Style.Container>
+    </Style.AccordionSection>
+  );
 };
 
 export default Accordion;
