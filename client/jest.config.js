3858f00da5b183e266dde0e3da95689933cba41f
mrvanagas <lukonelis1@gmail.com>
2022-06-15 14:45:56 +0300

    Added simple unit test

---
+++ client/jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    setupFilesAfterEnv: ['./src/utils/setupTests.js']
+  };
---
+++ client/src/Components/ContactForm/ContactForm.test.js
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+describe('ContactForm', () => {
+    test('should render inputs and button', () => {
+        render(<ContactForm />);
+        expect(screen.getByTestId('name-input')).toBeInTheDocument();
+        expect(screen.getByTestId('email-input')).toBeInTheDocument();
+        expect(screen.getByTestId('comments-input')).toBeInTheDocument();
+        expect(screen.getByTestId('button-submit')).toBeInTheDocument();
+    }
+    );
+
+    test('should not submit empty form', () => {
+        const onSubmit = jest.fn();
+        
+        render(<ContactForm onSubmit={onSubmit}/>);
+        const submitButton = screen.getByTestId('button-submit');
+
+        fireEvent.click(submitButton);
+        expect(onSubmit).not.toHaveBeenCalled();
+    }
+    );
+
+    test('should submit form', () => {
+        render(<ContactForm />);
+        const nameInput = screen.getByTestId('name-input');
+        const emailInput = screen.getByTestId('email-input');
+        const messageInput = screen.getByTestId('comments-input');
+        const submitButton = screen.getByTestId('button-submit');
+        fireEvent.change(nameInput, { target: { value: 'John' } });
+        fireEvent.change(emailInput, { target: { value: 'mail@doe.com' } });
+        fireEvent.change(messageInput, { target: { value: 'Hello this is a test message, thanks for reading!' } });
+        fireEvent.click(submitButton);
+    }
+    );
+});
+
+
+  
