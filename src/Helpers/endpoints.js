32bdf5d02ff193b7588787c669589fb6286b2c3d
vladleontescu <vladleontescu@coherentsolutions.com>
2022-06-24 10:14:37 +0300

    add weather symbol icon endpoint

--- src/Helpers/endpoints.js
+++ src/Helpers/endpoints.js
@@ -1,9 +1,7 @@
 const endpoints = {
     CURR_WEATHER: (coords) => `current/location=${coords.long},${coords.lat}`,
-    LOCATION_DATA: (coords) => `location/${coords.long},${coords.lat}`
-    // TODO FOR FUTURE USE
-    // DAILY_FORECAST_BY_ID: (id) => `forecast/daily/${locationId}`,
-    // HOURLY_FORECAST_BY_ID: (id) => `forecast/hourly/${locationId}`,
+    LOCATION_DATA: (coords) => `location/${coords.long},${coords.lat}`,
+    SYMBOL: (symbol) => `https://developer.foreca.com/static/images/symbols/${symbol}.png`
 };
 
 export default endpoints;
