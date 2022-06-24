c4f3de798780b05245d3382aeeaae0b6ac4a2b2e
vladleontescu <vladleontescu@coherentsolutions.com>
2022-06-24 10:16:08 +0300

    adjust style for navbar component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,17 +4,19 @@ import styles from './Navbar.module.scss';
 export default function Navbar() {
     return (
         <nav className={styles.navMenu}>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/cities">Cities</Link>
-                </li>
-            </ul>
+            <div className={styles.container}>
+                <ul className={styles['nav-items']}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/cities">Cities</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     );
 }
