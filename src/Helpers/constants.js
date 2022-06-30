9c7cc65c882fd86111bfe6b2786e04e8f1a7763d
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-06-30 10:08:31 +0300

    formatting

--- src/Helpers/constants.js
+++ src/Helpers/constants.js
@@ -2,22 +2,22 @@ const endpoints = {
   CURR_WEATHER: (coords) => `current/location=${coords.long},${coords.lat}`,
   LOCATION_DATA: (coords) => `location/${coords.long},${coords.lat}`,
   SYMBOL: (symbol) => `https://developer.foreca.com/static/images/symbols/${symbol}.png`,
-  DAILY_WEATHER: (coords) => `forecast/daily/${coords.long},${coords.lat}`,
+  DAILY_WEATHER: (coords) => `forecast/daily/${coords.long},${coords.lat}`
 };
 
 const opts = {
   dateShort: {
     day: 'numeric',
     weekday: 'short',
-    month: 'numeric',
+    month: 'numeric'
   },
   dateLong: {
     day: 'numeric',
     weekday: 'long',
-    month: 'long',
+    month: 'long'
   },
   year: { year: 'numeric' },
-  time: { hour: '2-digit', minute: '2-digit', hour12: false },
+  time: { hour: '2-digit', minute: '2-digit', hour12: false }
 };
 
 // const optsDate = {
