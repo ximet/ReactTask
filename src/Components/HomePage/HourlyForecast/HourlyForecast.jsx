40544686679d4eef134ca4b051a08050aa7ff746
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-07-05 20:40:38 +0300

    add hourly forecast component

---
+++ src/Components/HomePage/HourlyForecast/HourlyForecast.jsx
@@ -0,0 +1,40 @@
+import { getWeatherSymbol, formatDate } from '../../../Helpers/functions';
+import { opts } from '../../../Helpers/constants';
+
+import { WiRaindrop, WiStrongWind } from 'react-icons/wi';
+import styles from './HourlyForecast.module.scss';
+
+function HourlyForecast({ hourlyWeather }) {
+  const listItems = hourlyWeather.forecast.map((item) => {
+    return (
+      <div className={styles['container__slider-slide']} key={item.time}>
+        <div>
+          <strong>{formatDate(opts.time, item.time)}</strong>
+        </div>
+        <img width="40" src={getWeatherSymbol(item.symbol)} />
+        <div className={styles.temperature}>
+          <div className={styles['temperature']}>{item.temperature}&deg;</div>
+        </div>
+        <div className={styles.details}>
+          <small className={styles.rain}>
+            <WiRaindrop />
+            {item.precipProb}%
+          </small>
+          <small className={styles.wind}>
+            <WiStrongWind />
+            {item.windSpeed * 3.6} km/h
+          </small>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className={styles.container}>
+      <h2>Hourly</h2>
+      <div className={styles.container__slider}>{listItems}</div>
+    </div>
+  );
+}
+
+export default HourlyForecast;
--- src/Helpers/constants.js
+++ src/Helpers/constants.js
@@ -1,9 +1,11 @@
 const endpoints = {
-  CURR_WEATHER: (coords) => `current/location=${coords.long},${coords.lat}`,
-  LOCATION_DATA: (coords) => `location/${coords.long},${coords.lat}`,
+  CURR_WEATHER_BY_COORDS: (coords) => `current/location=${coords.long},${coords.lat}`,
+  CURR_WEATHER_BY_ID: (id) => `current/${id}`,
+  LOCATION_DATA_BY_COORDS: (coords) => `location/${coords.long},${coords.lat}`,
   LOCATION_NAME: (name) => `location/search/${name}`,
   SYMBOL: (symbol) => `https://developer.foreca.com/static/images/symbols/${symbol}.png`,
-  DAILY_WEATHER: (coords) => `forecast/daily/${coords.long},${coords.lat}`
+  DAILY_WEATHER: (coords) => `forecast/daily/${coords.long},${coords.lat}`,
+  HOURLY_WEATHER: (coords) => `forecast/hourly/${coords.long},${coords.lat}`
 };
 
 const opts = {
@@ -20,13 +22,5 @@ const opts = {
   year: { year: 'numeric' },
   time: { hour: '2-digit', minute: '2-digit', hour12: false }
 };
-
-// const optsDate = {
-//   day: 'numeric',
-//   weekday: 'long',
-//   month: 'long',
-// };
-// const optsYear = { year: 'numeric' };
-// const optsTime = { hour: '2-digit', minute: '2-digit', hour12: false };
 
 export { endpoints, opts };
