e07dfc195593589ee6e244c624005e9105afe6c1
Kolyo_Peev <kolyopeev@coherentsolutions.com>
2022-07-07 14:08:37 +0300

    clear

--- proxy.js
+++ proxy.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const axios = require('axios').default;
 const cors = require('cors');
-const { json } = require('express');
 require('dotenv').config();
 
 let getApiKey;
