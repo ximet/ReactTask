3ef8c80f62d26c2e7c3bd254d801f098aafb500f
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-07-12 15:41:39 +0300

    remove isSubmit state move cross icon to Error Message Components replace br tag with css

--- src/Components/FeedbackPage/FeedbackForm/ErrorMessage.jsx
+++ src/Components/FeedbackPage/FeedbackForm/ErrorMessage.jsx
@@ -1,8 +1,7 @@
-import { BiErrorCircle } from 'react-icons/bi';
 import styles from './ErrorMessage.module.scss';
 
 function ErrorMessage({ message }) {
-  return <small className={styles.message}>{message}</small>;
+  return <small className={styles.message}>{message && `❌ ${message}`}</small>;
 }
 
 export default ErrorMessage;
--- src/Components/FeedbackPage/FeedbackForm/FeedbackForm.jsx
+++ src/Components/FeedbackPage/FeedbackForm/FeedbackForm.jsx
@@ -16,7 +16,6 @@ function FeedbackForm() {
   };
   const [formData, setFormData] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit] = useState(false);
 
   function handleChange(event) {
     const { name, value, type, checked } = event.target;
@@ -31,11 +30,10 @@ function FeedbackForm() {
   function handleSubmit(event) {
     event.preventDefault();
     setFormErrors(validate(formData));
-    setIsSubmit(true);
   }
 
   function saveToLocalStorage() {
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
+    if (Object.keys(formErrors).length === 0) {
       saveData(formData);
     }
   }
@@ -50,25 +48,25 @@ function FeedbackForm() {
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (!values.username) {
-      errors.username = '❌ Name is required!';
+      errors.username = 'Name is required!';
     } else if (values.username.length < 3) {
-      errors.username = '❌ Must be at least 3 characters';
+      errors.username = 'Must be at least 3 characters';
     }
     if (!values.email) {
-      errors.email = '❌ Email is required!';
+      errors.email = 'Email is required!';
     } else if (!emailRegEx.test(values.email)) {
-      errors.email = '❌ This is not a valid email format!';
+      errors.email = 'This is not a valid email format!';
     }
     if (!values.comments) {
-      errors.comments = '❌ Please leave a comment!';
-    } else if (!values.comments.length < 20) {
-      errors.comments = '❌ Must be at least 20 characters';
+      errors.comments = 'Please leave a comment!';
+    } else if (values.comments.length < 20) {
+      errors.comments = 'Must be at least 20 characters';
     }
     if (!values.feature) {
-      errors.feature = '❌ Please select a feature!';
+      errors.feature = 'Please select a feature!';
     }
     if (values.feedbackType === 'selection' || !values.feedbackType) {
-      errors.feedbackType = '❌ Please select feedback type!';
+      errors.feedbackType = 'Please select feedback type!';
     }
     return errors;
   }
@@ -132,49 +130,54 @@ function FeedbackForm() {
 
         <fieldset>
           <legend>What feature is your feedback about?</legend>
-          <input
-            type="radio"
-            id="currentConditions"
-            name="feature"
-            value="Current Conditions"
-            checked={formData.feature === 'Current Conditions'}
-            onChange={handleChange}
-          />
-          <label htmlFor="currentConditions">Current Conditions</label>
-          <br />
 
-          <input
-            type="radio"
-            id="forecast"
-            name="feature"
-            value="Forecast"
-            checked={formData.feature === 'Forecast'}
-            onChange={handleChange}
-          />
-          <label htmlFor="forecast">Forecast</label>
-          <br />
+          <label htmlFor="currentConditions" className={styles['fieldset-group']}>
+            <input
+              type="radio"
+              id="currentConditions"
+              name="feature"
+              value="Current Conditions"
+              checked={formData.feature === 'Current Conditions'}
+              onChange={handleChange}
+            />
+            Current Conditions
+          </label>
 
-          <input
-            type="radio"
-            id="locations"
-            name="feature"
-            value="List of locations"
-            checked={formData.feature === 'List of locations'}
-            onChange={handleChange}
-          />
-          <label htmlFor="locations">List of locations</label>
-          <br />
+          <label htmlFor="forecast" className={styles['fieldset-group']}>
+            <input
+              type="radio"
+              id="forecast"
+              name="feature"
+              value="Forecast"
+              checked={formData.feature === 'Forecast'}
+              onChange={handleChange}
+            />
+            Forecast
+          </label>
 
-          <input
-            type="radio"
-            id="other"
-            name="feature"
-            value="Other"
-            checked={formData.feature === 'Other'}
-            onChange={handleChange}
-          />
-          <label htmlFor="other">Other</label>
-          <br />
+          <label htmlFor="locations" className={styles['fieldset-group']}>
+            <input
+              type="radio"
+              id="locations"
+              name="feature"
+              value="List of locations"
+              checked={formData.feature === 'List of locations'}
+              onChange={handleChange}
+            />
+            List of locations
+          </label>
+
+          <label htmlFor="other" className={styles['fieldset-group']}>
+            <input
+              type="radio"
+              id="other"
+              name="feature"
+              value="Other"
+              checked={formData.feature === 'Other'}
+              onChange={handleChange}
+            />
+            Other
+          </label>
           <ErrorMessage message={formErrors.feature} />
         </fieldset>
 
