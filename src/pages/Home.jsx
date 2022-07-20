f8109c8474d4c158755cfcc922c554376cac534b
Kolyo-Coherent <107244800+Kolyo-Coherent@users.noreply.github.com>
2022-07-20 12:52:40 +0300

    Fe nav and pages initiallization (#245)
    * nice try
    
    * proxy implemented and files cleaned
    
    * removed unused files, dependencies and logs
    
    * added pages and search functionality
    
    * more changes
    
    * cleaning pull request
    
    * clear
    
    * updated pull request
    
    * updated pull request
    
    * last updatete to this branch hopefully

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar';
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <SearchBar />
+    </>
+  );
+}
