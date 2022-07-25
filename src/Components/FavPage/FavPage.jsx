fc0b8790411b6a81c61abe427b46b8be98c22558
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-07-25 13:06:47 +0300

    refactor Message component and rendering

--- src/Components/FavPage/FavPage.jsx
+++ src/Components/FavPage/FavPage.jsx
@@ -10,25 +10,25 @@ function FavPage() {
     return <FavCitiesList data={item} key={item.locationData.id} />;
   });
 
-  function renderComponent() {
-    if (listItems.length !== 0) {
-      return (
-        <main className={styles.container}>
+  return (
+    <main className={styles.container}>
+      {listItems.length !== 0 ? (
+        <>
           <h2 className={styles.title}>Favourite Locations</h2>
           <div className={styles['container-items']}>{listItems}</div>
-        </main>
-      );
-    } else {
-      return (
-        <Message
-          message1={'Here you can see a list of the weather from your favourite cities.'}
-          message2={'Your list is empty. Search for a city and add it as a favourite!'}
-        />
-      );
-    }
-  }
-
-  return renderComponent();
+        </>
+      ) : (
+        <>
+          <Message>
+            <p>Here you can see a list of the weather from your favourite cities.</p>
+            <br />
+            <br />
+            <p>Your list is empty. Search for a city and add it as a favourite!</p>
+          </Message>
+        </>
+      )}
+    </main>
+  );
 }
 
 export default FavPage;
