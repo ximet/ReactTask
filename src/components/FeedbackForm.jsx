c8a755bff95abbc04440fdda8f988d4aaa81d768
Kolyo_Peev <kolyopeev@coherentsolutions.com>
2022-08-02 18:08:35 +0300

    updates to form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -17,8 +17,8 @@ export default function FeedbackForm() {
   };
 
   const radioSelected = e => {
-    setFormValues({ ...formValues, rating: e.target.value })
-  }
+    setFormValues({ ...formValues, rating: e.target.value });
+  };
 
   const resetForm = data => {
     if (data.status === 200) {
@@ -31,7 +31,7 @@ export default function FeedbackForm() {
       });
       setSubmitted(false);
       setValidity(false);
-      Array.from(document.getElementsByName('rating')).map(item => item.checked = false);
+      Array.from(document.getElementsByName('rating')).map(item => (item.checked = false));
     } else {
       alert('Something went wrong! Please try again.');
     }
@@ -63,7 +63,9 @@ export default function FeedbackForm() {
             value={formValues.firstName}
             onChange={changeVal}
           />
-          {submitted && !formValues.firstName && <span className="error-msg">Name is empty or invalid!</span>}
+          {submitted && !formValues.firstName && (
+            <span className="error-msg">Name is empty or invalid!</span>
+          )}
         </fieldset>
         <fieldset>
           <label htmlFor="lastName">Last name:</label>
@@ -74,54 +76,94 @@ export default function FeedbackForm() {
             value={formValues.lastName}
             onChange={changeVal}
           />
-          {submitted && !formValues.lastName && <span className="error-msg">Last name is empty or invalid!</span>}
+          {submitted && !formValues.lastName && (
+            <span className="error-msg">Last name is empty or invalid!</span>
+          )}
         </fieldset>
         <fieldset>
           <label htmlFor="mail">E-mail:</label>
           <input type="email" id="mail" name="mail" value={formValues.mail} onChange={changeVal} />
-          {submitted && !formValues.mail && <span className="error-msg">E-mail is empty or invalid!</span>}
+          {submitted && !formValues.mail && (
+            <span className="error-msg">E-mail is empty or invalid!</span>
+          )}
         </fieldset>
         <fieldset>
           <label htmlFor="satisfactory-index">Rate our services</label>
-          <div className='radio-set-container'>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex1"
-                name="rating" value="super-bad" onChange={radioSelected} />
+          <div className="radio-set-container">
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex1"
+                name="rating"
+                value="super-bad"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex1">&#128169;</label>
             </div>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex2"
-                name="rating" value="bad" onChange={radioSelected} />
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex2"
+                name="rating"
+                value="bad"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex2">&#129314;</label>
             </div>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex3"
-                name="rating" value="mediocre" onChange={radioSelected} />
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex3"
+                name="rating"
+                value="mediocre"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex3">&#128547;</label>
             </div>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex4"
-                name="rating" value="OK" onChange={radioSelected} />
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex4"
+                name="rating"
+                value="OK"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex4">&#129300;</label>
             </div>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex5"
-                name="rating" value="good" onChange={radioSelected} />
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex5"
+                name="rating"
+                value="good"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex5">&#128522;</label>
             </div>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex6"
-                name="rating" value="very-good" onChange={radioSelected} />
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex6"
+                name="rating"
+                value="very-good"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex6">&#x1F60D;</label>
             </div>
-            <div className='radio-set'>
-              <input type="radio" id="satisfactoryIndex7"
-                name="rating" value="excellent" onChange={radioSelected} />
+            <div className="radio-set">
+              <input
+                type="radio"
+                id="satisfactoryIndex7"
+                name="rating"
+                value="excellent"
+                onChange={radioSelected}
+              />
               <label htmlFor="satisfactoryIndex7">&#127866;</label>
             </div>
-            {submitted && !formValues.rating && <span className="error-msg">You didn't rate us!</span>}
+            {submitted && !formValues.rating && (
+              <span className="error-msg">You didn't rate us!</span>
+            )}
           </div>
-
         </fieldset>
         <fieldset>
           <label htmlFor="feedback">Please leave your feedback below</label>
