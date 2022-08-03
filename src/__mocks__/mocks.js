166e686139c8842546f60c2a6b916b3c20980932
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-08-03 17:36:39 +0300

    add test and edit package.json
    - add 2 more test cases (diff. actions provided)
    - remove  "regenerator-runtime": "^0.13.9"

---
+++ src/__mocks__/mocks.js
@@ -0,0 +1,36 @@
+export const mockCityData = [
+  {
+    currWeather: {
+      cloudiness: 77,
+      dewPoint: 16,
+      feelsLikeTemp: 26,
+      precipProb: 12,
+      precipRate: 0,
+      pressure: 1013.44,
+      relHumidity: 55,
+      symbol: 'd300',
+      symbolPhrase: 'cloudy',
+      temperature: 26,
+      thunderProb: 0,
+      time: '2022-08-02T11:11+03:00',
+      uvIndex: 5,
+      visibility: 10285,
+      windDir: 353,
+      windDirString: 'N',
+      windGust: 5,
+      windSpeed: 1
+    },
+    locationData: {
+      adminArea: 'București',
+      adminArea2: 'Municipiul Bucureşti',
+      adminArea3: 'Sector 3',
+      country: 'Romania',
+      id: 100683506,
+      language: 'en',
+      lat: 44.4323,
+      lon: 26.1063,
+      name: 'Bucharest',
+      timezone: 'Europe/Bucharest'
+    }
+  }
+];
--- src/__tests__/locations.test.js
+++ src/__tests__/locations.test.js
@@ -1,9 +1,24 @@
 import cityReducer from '../redux/locations';
+import mockCityData from '../__mocks__/mocks';
 
 describe('theme reducer', () => {
   it('should return the initial state when passed an empty action', () => {
     const favCityList = [];
     const action = { type: '' };
+    const result = cityReducer(favCityList, action);
+    expect(result).toEqual([]);
+  });
+  it('should save city data object given initial state', () => {
+    const favCityList = [];
+    const cityName = 'Bucharest';
+    const action = { type: 'SAVE_FAV_CITY', payload: cityName };
+    const result = cityReducer(favCityList, action);
+    expect(result).toEqual(['Bucharest']);
+  });
+  it('should delete city data object', () => {
+    const favCityList = mockCityData;
+    const cityName = 'Bucharest';
+    const action = { type: 'DELETE_FAV_CITY', payload: cityName };
     const result = cityReducer(favCityList, action);
     expect(result).toEqual([]);
   });
