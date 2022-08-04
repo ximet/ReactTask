bcaf26f4b0bc738bbce44d53a7d5a7d60fcf4b43
Vlad Leontescu <vladleontescu@coherentsolutions.com>
2022-08-04 14:52:40 +0300

    fix test case #4 and add test case #5

--- src/__mocks__/mocks.js
+++ src/__mocks__/mocks.js
@@ -32,5 +32,39 @@ export const mockCityData = [
       name: 'Bucharest',
       timezone: 'Europe/Bucharest'
     }
+  },
+  {
+    currWeather: {
+      cloudiness: 0,
+      dewPoint: 20,
+      feelsLikeTemp: 33,
+      precipProb: 2,
+      precipRate: 0,
+      pressure: 1014.72,
+      relHumidity: 52,
+      symbol: 'd000',
+      symbolPhrase: 'clear',
+      temperature: 31,
+      thunderProb: 0.188042,
+      time: '2022-08-04T13:50+02:00',
+      uvIndex: 8,
+      visibility: 14781,
+      windDir: 187,
+      windDirString: 'S',
+      windGust: 9,
+      windSpeed: 5
+    },
+    locationData: {
+      adminArea: 'Catalonia',
+      adminArea2: 'Barcelona',
+      adminArea3: 'Barcelona',
+      country: 'Spain',
+      id: 103128760,
+      language: 'en',
+      lat: 41.3888,
+      lon: 2.159,
+      name: 'Barcelona',
+      timezone: 'Europe/Madrid'
+    }
   }
 ];
