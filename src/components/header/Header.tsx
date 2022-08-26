1df8293dbd3c0b2a3ff7316d3a64da5da6d817e0
Andrei Shulha <AndreiShulha@coherentsolutions.com>
2022-08-26 16:22:10 +0300

    add style for active link and css variables

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,28 +3,35 @@ import styles from './Header.css';
 import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
+type LinkType = {
+  isActive: boolean;
+};
+
+const setActive = ({ isActive }: LinkType) =>
+  isActive ? `${styles.listItem} ${styles['active-link']}` : styles.listItem;
+
 const Header: FC = () => {
   return (
     <header className={styles.header}>
       <nav>
         <ul className={styles.navList}>
           <li>
-            <NavLink to="/" className={styles.listItem}>
+            <NavLink to="/" className={setActive}>
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink to="/countries" className={styles.listItem}>
+            <NavLink to="/countries" className={setActive}>
               Countries
             </NavLink>
           </li>
           <li>
-            <NavLink to="/details" className={styles.listItem}>
+            <NavLink to="/details" className={setActive}>
               Details
             </NavLink>
           </li>
           <li>
-            <NavLink to="/feedback" className={styles.listItem}>
+            <NavLink to="/feedback" className={setActive}>
               Feedback
             </NavLink>
           </li>
