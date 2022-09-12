1552c6e199be81f8a946c2bf56a3a984d5e32dfb
Silvik95 <igor.silvestru.work@gmail.com>
2022-09-12 14:56:04 +0300

    anothers small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,17 @@ import styles from '../styles.scss';
 
 function Footer() {
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
-  const interval = setInterval(() => {
-    setCurrentTime(new Date().toLocaleTimeString());
-  }, 1000);
   useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
     return () => {
-      console.log('test');
       clearInterval(interval);
     };
   }, []);
   return (
     <div className={styles.appfooter}>
-      <h3>{currentTime}</h3>
+      <p>{currentTime}</p>
     </div>
   );
 }
