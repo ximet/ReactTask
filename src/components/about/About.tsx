0dbb415d820a187f4a2e9b3d15916b03d63de392
Andrei Shulha <AndreiShulha@coherentsolutions.com>
2022-09-14 14:27:42 +0300

    del p tag and add br

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -9,28 +9,24 @@ export const About: FC = () => {
       <h2 className={styles['about-title']}>More info about our service</h2>
       <article className={styles['about-article']}>
         <h3 className={styles['about-article-title']}>What you can?</h3>
-        <p> By default, you can see the weather in your city. </p>
-        <p> You can also find the city you need and find out what the weather is like there.</p>
-        <p> If you need a city all the time, you can add it to your favorites.</p>
+        <p>
+          By default, you can see the weather in your city. <br />
+          You can also find the city you need and find out what the weather is like there. <br />
+          If you need a city all the time, you can add it to your favorites.
+        </p>
       </article>
       <article className={styles['about-article']}>
         <h3 className={styles['about-article-title']}>How work with service?</h3>
         <p>
-          The coolest thing is that you don't need to register.
-          <p>
-            To find the desired city, enter it in the search bar and click on the desired result.
-          </p>
-          <p> To add a city to your favorites, click on the heart icon.</p>
-          <p> You can also click on this icon to remove the city from your favorites. </p>
-          <p>
-            {' '}
-            On the favorites page, click on the desired city to view detailed weather in that city.{' '}
-          </p>
-          <p>
-            {' '}
-            On the main page with detailed weather information, select the number of days to see the
-            weather for the future.{' '}
-          </p>
+          The coolest thing is that you don't need to register. <br />
+          To find the desired city, enter it in the search bar and click on the desired result.{' '}
+          <br />
+          To add a city to your favorites, click on the heart icon. <br />
+          You can also click on this icon to remove the city from your favorites. <br />
+          On the favorites page, click on the desired city to view detailed weather in that city.{' '}
+          <br />
+          On the main page with detailed weather information, select the number of days to see the
+          weather for the future.
         </p>
       </article>
       <article className={styles['about-article']}>
