42acc80a4b932539b08e01c74131e0cee7d66879
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-09-21 17:19:44 +0300

    created hook useSearch

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import useSearch from '../hooks/useSearch';
 import styles from '../styles.scss';
 import Button from '../UI/button/Button';
 import Input from '../UI/input/Input';
@@ -15,12 +16,7 @@ export default function MainPage() {
   }
 
   useEffect(() => {
-    fetch(`https://pfa.foreca.com/api/v1/location/search/${searchQuery}`, {
-      headers: {
-        Authorization:
-          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY2MzM2MTk2NywiZXhwIjo5OTk5OTk5OTk5LCJuYmYiOjE2NjMzNjE5NjcsImp0aSI6IjUxNDc0OTI0Yjc3NTQxMDkiLCJzdWIiOiJpZ29yaW9oYTk1IiwiZm10IjoiWERjT2hqQzQwK0FMamxZVHRqYk9pQT09In0.fZfEoqlYCWc4qs6-muuStu-ZCITDxeN5Qpg2705Sb00'
-      }
-    })
+    useSearch(searchQuery)
       .then(res => res.json())
       .then(cityes => setItems(cityes.locations))
       .catch(error => {
