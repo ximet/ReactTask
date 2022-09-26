9a70e501ff3d22d65eb8ad63b3e36585b0ce05ae
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-09-26 14:32:32 +0300

    implementing previos hooks in code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainPage from './components/MainPage';
+import useAutorisation from './hooks/useAutorisation';
 
 function App() {
+  useAutorisation();
   return (
     <>
       <Header />
