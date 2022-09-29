9cc083bedd96f8a2a8921d2afc666951deb475ba
ViliusGaizutis <viliusgaizutis@coherentsolutions.com>
2022-09-29 17:02:22 +0300

    Moved Header back to Layout component

--- client/src/components/AboutPage/AboutPage.tsx
+++ client/src/components/AboutPage/AboutPage.tsx
@@ -1,20 +1,16 @@
 import React, { FunctionComponent } from 'react';
 
 // Components
-import Header from 'components/Header/Header';
 import AboutHeroSection from 'components/AboutPage/AboutHeroSection/AboutHeroSection';
 import AboutHowSection from 'components/AboutPage/AboutHowSection/AboutHowSection';
 import AboutFeedbackSection from 'components/AboutPage/AboutFeedbackSection/AboutFeedbackSection';
 
 const AboutPage: FunctionComponent = () => (
-  <>
-    <Header />
-    <main>
-      <AboutHeroSection />
-      <AboutHowSection />
-      <AboutFeedbackSection />
-    </main>
-  </>
+  <main>
+    <AboutHeroSection />
+    <AboutHowSection />
+    <AboutFeedbackSection />
+  </main>
 );
 
 export default AboutPage;
--- client/src/components/LocationPage/LocationPage.tsx
+++ client/src/components/LocationPage/LocationPage.tsx
@@ -1,20 +1,16 @@
 import React, { FunctionComponent } from 'react';
 
 // Components
-import Header from 'components/Header/Header';
 import Dashboard from 'components/Dashboard/Dashboard';
 
 // Styles
 import { Container } from 'styles/global';
 
 const LocationPage: FunctionComponent = () => (
-  <>
-    <Header />
-    <main>
-      <Container>
-        <Dashboard />
-      </Container>
-    </main>
-  </>
+  <main>
+    <Container>
+      <Dashboard />
+    </Container>
+  </main>
 );
 export default LocationPage;
