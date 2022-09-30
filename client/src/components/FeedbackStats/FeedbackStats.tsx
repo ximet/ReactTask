a9d21cefcb0c84e54d6637de50b327e156bce150
Aureja Slance <aurejaslance@coherentsolutions.com>
2022-09-30 12:20:01 +0300

    initial: feedback list and stats components

---
+++ client/src/components/FeedbackStats/FeedbackStats.tsx
@@ -0,0 +1,29 @@
+import InputRating from 'components/InputRating/InputRating';
+import FeedbackContext from 'contexts/FeedbackContext';
+import React, { useContext } from 'react';
+import { calculateFeedbackAvg } from 'utils/calculateFeedbackAvg';
+import styles from './FeedbackStats.module.scss';
+
+const FeedbackStats = () => {
+  const { feedback } = useContext(FeedbackContext);
+
+  return (
+    <div className={styles.container}>
+      <h4>{feedback.length} Reviews</h4>
+      <div className={styles.statsContainer}>
+        <InputRating
+          type="hidden"
+          name="postRating"
+          id="postNum"
+          value={Math.round(calculateFeedbackAvg(feedback))}
+          onChange={() => {
+            return null;
+          }}
+        />
+        <h4 className={styles.ratingAvg}>{calculateFeedbackAvg(feedback).toFixed(2)}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackStats;
