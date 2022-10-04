98e59df96c458a99eb3ea66f0f07679fe6a6eaa1
Andrei Shulha <AndreiShulha@coherentsolutions.com>
2022-10-04 18:00:26 +0300

    change getHourLabels function and add label on UI

--- src/components/main/HourlySection.tsx
+++ src/components/main/HourlySection.tsx
@@ -1,29 +1,46 @@
-import React, { FC, useMemo, useState } from 'react';
+import React, { FC, useMemo, useState, useRef, ChangeEvent } from 'react';
 import { convertTime } from 'utils/helpers';
 import styles from './Main.css';
 import MainHourlyCard from './MainHourlyCard';
 import { HourlyWeatherType } from 'types/weatherTypes';
+import classNames from 'classnames';
 
 type HourlySectionProps = {
   weather: HourlyWeatherType[];
 };
 
-const getHourLabels = (weather: HourlyWeatherType[]): string[] => {
+const CORRECT_POSITION_COEFFICIENT = 4.2;
+
+const getHourLabels = (weather: HourlyWeatherType[], middleLabelsCount: number): string[] => {
   const labels: string[] = [];
 
-  weather.forEach((el: HourlyWeatherType, i: number) => {
-    if (i === 0 || i === 7 || i === 15 || i === 23) {
-      const hours = convertTime(el.time).hours;
-      const minutes = convertTime(el.time).minutes;
-      labels.push(`${hours}:${minutes}`);
-    }
-  });
+  const step = weather.length / (middleLabelsCount + 1);
+
+  const firstEl = weather.at(0);
+  const lastEl = weather.at(-1);
+
+  labels.push(`
+      ${convertTime(firstEl!.time).hours}:${convertTime(firstEl!.time).minutes}
+    `);
+
+  for (let i = step - 1; i < weather.length - step; i = i + step) {
+    const hours = convertTime(weather[i].time).hours;
+    const minutes = convertTime(weather[i].time).minutes;
+    labels.push(`${hours}:${minutes}`);
+  }
+
+  labels.push(`
+      ${convertTime(lastEl!.time).hours}:${convertTime(lastEl!.time).minutes}
+    `);
 
   return labels;
 };
 
 const HourlySection: FC<HourlySectionProps> = ({ weather }) => {
   const [hour, setHour] = useState<number>(0);
+  const inputLabelRef = useRef<HTMLSpanElement | null>(null);
+  const { current: inputLabel } = inputLabelRef;
+  const timeoutId = useRef<number>(0);
 
   const hoursSchema = useMemo(
     () =>
@@ -38,18 +55,40 @@ const HourlySection: FC<HourlySectionProps> = ({ weather }) => {
     (el: HourlyWeatherType) => +convertTime(el.time).hours === hoursSchema[hour]
   );
 
-  const hourLabels = useMemo(() => getHourLabels(weather), [weather]);
+  const hourLabels = useMemo(() => getHourLabels(weather, 2), [weather]);
+
+  const date = convertTime(currentWeatherByHour!.time);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value: number = e.target.valueAsNumber;
+
+    clearTimeout(timeoutId.current);
+    setHour(value);
+
+    if (inputLabel) {
+      inputLabel.classList.add(classNames(styles['show']));
+      inputLabel.style.left = value * CORRECT_POSITION_COEFFICIENT + '%';
+      timeoutId.current = window.setTimeout(() => {
+        inputLabel.classList.remove(classNames(styles['show']));
+      }, 1000);
+    }
+  };
 
   return (
     <div className={styles['section-hourly']}>
       <MainHourlyCard hourlyWeather={currentWeatherByHour!} />
+      <div className={styles['hourly-input-value']}>
+        <span ref={inputLabelRef}>
+          {date.hours}:{date.minutes}
+        </span>
+      </div>
       <input
         type="range"
         className={styles['hourly-input-range']}
         value={hour}
         min={0}
         max={23}
-        onChange={e => setHour(e.target.valueAsNumber)}
+        onChange={onChangeHandler}
       />
       <div className={styles['hour-labels']}>
         {hourLabels.map(el => (
