6ca9f01c5dc65c014f3408f8241bd11bbccea3aa
Andrew Shulga <78097819+AndrewHelix@users.noreply.github.com>
2022-10-05 12:25:33 +0300

    Added main label in UI, change function for get labels (#320)
    * add layout
    
    * add layout
    
    * add router and custom hook for position
    
    * add style for active link and css variables
    
    * fix: get position in main component
    
    * add request weather and show result
    
    * add request weather and show result
    
    * fix mistakes
    
    * fix mistakes
    
    * del default ip server
    
    * fix url: now work:)
    
    * add more info in current weather and refactor
    
    * fix mistakes
    
    * fix mistakes
    
    * add more info in main page
    
    * fix mistakes and change styles
    
    * add select days  and search
    
    * fix mistakes
    
    * add context and reducer
    
    * fix mistakes
    
    * fix mistakes
    
    * add night theme and countries page
    
    * fix mistakes
    
    * fix mistakes
    
    * add info in about page and rederect from countries page
    
    * del p tag and add br
    
    * add feedback form and validation
    
    * refactor feedback reducer
    
    * move feedback reducer to reducers folder
    
    * move feedback reducer to reducers folder
    
    * fix mistakes
    
    * refactor useReducer type and classNames
    
    * add optimization
    
    * add position in url
    
    * fix mistake
    
    * fix mistake
    
    * add graph view for daily and hourly weather
    
    * add graph view for daily and hourly weather
    
    * merge with parent
    
    * change main graph and fix mistake
    
    * changed to ternary operator
    
    * add adaptive
    
    * change feedback UI and refactor validation
    
    * fix mistakes
    
    * fix format
    
    * fix mistakes
    
    * del console.log
    
    * change view for hourly weather
    
    * del comment
    
    * fix mistakes
    
    * change getHourLabels function and add label on UI
    
    * refactor: round step and create gettimelibel function
    
    Co-authored-by: Andrei Shulha <AndreiShulha@coherentsolutions.com>

--- src/components/main/HourlySection.tsx
+++ src/components/main/HourlySection.tsx
@@ -1,29 +1,46 @@
-import React, { FC, useMemo, useState } from 'react';
+import React, { FC, useMemo, useState, useRef, ChangeEvent } from 'react';
 import { convertTime } from 'utils/helpers';
 import styles from './Main.css';
 import MainHourlyCard from './MainHourlyCard';
 import { HourlyWeatherType } from 'types/weatherTypes';
+import classNames from 'classnames';
 
 type HourlySectionProps = {
   weather: HourlyWeatherType[];
 };
 
-const getHourLabels = (weather: HourlyWeatherType[]): string[] => {
+const CORRECT_POSITION_COEFFICIENT = 4.2;
+
+const getTimeLabel = (time: string): string => {
+  const date = convertTime(time);
+
+  return `${date.hours}:${date.minutes}`;
+};
+
+const getHourLabels = (weather: HourlyWeatherType[], middleLabelsCount: number): string[] => {
   const labels: string[] = [];
 
-  weather.forEach((el: HourlyWeatherType, i: number) => {
-    if (i === 0 || i === 7 || i === 15 || i === 23) {
-      const hours = convertTime(el.time).hours;
-      const minutes = convertTime(el.time).minutes;
-      labels.push(`${hours}:${minutes}`);
-    }
-  });
+  const step = Math.floor(weather.length / (middleLabelsCount + 1));
+
+  const firstEl = weather.at(0);
+  const lastEl = weather.at(-1);
+
+  labels.push(getTimeLabel(firstEl!.time));
+
+  for (let i = step - 1; i < weather.length - step; i = i + step) {
+    labels.push(getTimeLabel(weather[i].time));
+  }
+
+  labels.push(getTimeLabel(lastEl!.time));
 
   return labels;
 };
 
 const HourlySection: FC<HourlySectionProps> = ({ weather }) => {
   const [hour, setHour] = useState<number>(0);
+  const inputLabelRef = useRef<HTMLSpanElement | null>(null);
+  const { current: inputLabel } = inputLabelRef;
+  const timeoutId = useRef<number>(0);
 
   const hoursSchema = useMemo(
     () =>
@@ -38,18 +55,36 @@ const HourlySection: FC<HourlySectionProps> = ({ weather }) => {
     (el: HourlyWeatherType) => +convertTime(el.time).hours === hoursSchema[hour]
   );
 
-  const hourLabels = useMemo(() => getHourLabels(weather), [weather]);
+  const hourLabels = useMemo(() => getHourLabels(weather, 2), [weather]);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value: number = e.target.valueAsNumber;
+
+    clearTimeout(timeoutId.current);
+    setHour(value);
+
+    if (inputLabel) {
+      inputLabel.classList.add(classNames(styles['show']));
+      inputLabel.style.left = value * CORRECT_POSITION_COEFFICIENT + '%';
+      timeoutId.current = window.setTimeout(() => {
+        inputLabel.classList.remove(classNames(styles['show']));
+      }, 1000);
+    }
+  };
 
   return (
     <div className={styles['section-hourly']}>
       <MainHourlyCard hourlyWeather={currentWeatherByHour!} />
+      <div className={styles['hourly-input-value']}>
+        <span ref={inputLabelRef}>{getTimeLabel(currentWeatherByHour!.time)}</span>
+      </div>
       <input
         type="range"
         className={styles['hourly-input-range']}
         value={hour}
         min={0}
         max={23}
-        onChange={e => setHour(e.target.valueAsNumber)}
+        onChange={onChangeHandler}
       />
       <div className={styles['hour-labels']}>
         {hourLabels.map(el => (
