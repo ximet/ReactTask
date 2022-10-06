033b43d8eb10a8334b968113f6610a29567b26f0
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-10-06 14:33:54 +0300

    logo added in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,12 @@ import { Link } from 'react-router-dom';
 import styles from '../styles.scss';
 function Header() {
   return (
-    <div className={styles.appheader}>
-      <Link to="/">
-        <p>CanYouSwim??</p>
+    <nav className={styles.appheader}>
+      <Link className={styles.logo} to="/">
+        CanYouSwim??
       </Link>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </div>
+      <Link to="/about">About</Link>
+    </nav>
   );
 }
 export default Header;
