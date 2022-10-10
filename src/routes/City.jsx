86c8b48057c4bef58ba2377434dee875a59c9e96
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-10-10 16:06:19 +0300

    temperature added

--- src/routes/City.jsx
+++ src/routes/City.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles.scss';
 
 function City() {
+  const [cityInfo, setCityInfo] = useState([]);
   const city = JSON.parse(localStorage.getItem('currentCity'));
-  return <div className={styles.cityPage}>Now you are loocking on information of {city.name}</div>;
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `http://localhost:3333/pfa.foreca.com/api/v1/current/${city.id}`
+      );
+      const reqData = await response.json();
+      setCityInfo(reqData.current);
+    })();
+  }, []);
+  console.log(cityInfo);
+  return (
+    <div className={styles.cityPage}>
+      the current weather in {city.name} is {cityInfo.temperature} deg'
+    </div>
+  );
 }
 export default City;
