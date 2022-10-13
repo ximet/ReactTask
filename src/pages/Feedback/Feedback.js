a81c1aae8f3024c835b489af72bddfd70c0bbdc8
P N <petyanikleva@yahoo.com>
2022-10-13 14:59:24 +0300

    Implement saving the feedback comments to  the local storage

--- src/pages/Feedback/Feedback.js
+++ src/pages/Feedback/Feedback.js
@@ -1,4 +1,7 @@
+import { useState } from 'react';
 import styles from './Feedback.module.css';
+
+import { FEEDBACK, SUMBIT_LABEL } from '../../components/constants';
 
 import {
   title,
@@ -6,42 +9,69 @@ import {
   question1,
   question2,
   question3,
-  question4,
-  question5
+  question4
 } from './feedbackQuestions';
 
 const Feedback = () => {
+  const [answer3, setAnswer3] = useState('');
+  const [answer4, setAnswer4] = useState('');
+
+  const handleAnswer3 = e => setAnswer3(e.target.value);
+  const handleAnswer4 = e => setAnswer4(e.target.value);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const answer1 = formData.get('question1');
+    const answer2 = formData.get('question2');
+    const answer3 = formData.get('question3');
+    const answer4 = formData.get('question4');
+    const feedbackComments = {
+      [question1.question]: answer1,
+      [question2.question]: answer2,
+      [question3]: answer3,
+      [question4]: answer4
+    };
+
+    let arrayFeedback = [feedbackComments];
+    const prevFeedback = JSON.parse(localStorage.getItem(FEEDBACK));
+    if (prevFeedback) {
+      arrayFeedback = [...prevFeedback, feedbackComments];
+    }
+    localStorage.setItem(FEEDBACK, JSON.stringify(arrayFeedback));
+  };
+
   return (
-    <form id="form">
+    <form id="form" onSubmit={submitHandler}>
       <h2>{title}</h2>
       <h2 className={styles.titleSecondary}>{titleSecondary}</h2>
 
       <div className={styles.formControl}>
         <label htmlFor="qustion1" id="qustion1">
-          {question1}
+          {question1.question}
         </label>
 
         <select name="question1" id="question1">
           <option value="-"> </option>
-          <option value="oncePerMonth">Once per month</option>
-          <option value="oncePerWeek">Once or twice per week</option>
-          <option value="everyday">Everyday</option>
-          <option value="other">Other</option>
+          <option value={question1.option1}>{question1.option1}</option>
+          <option value={question1.option2}>{question1.option2}</option>
+          <option value={question1.option3}>{question1.option3}</option>
+          <option value={question1.option4}>{question1.option4}</option>
         </select>
       </div>
 
       <div className={styles.formControl}>
         <label htmlFor="question2" id="question2">
-          {question2}
+          {question2.question}
         </label>
 
         <select name="question2" id="question2">
           <option value="-"> </option>
-          <option value="1">1 - don't like it at all.</option>
-          <option value="2">2 - it could be better.</option>
-          <option value="3">3 - can't decide.</option>
-          <option value="4">4 - like it.</option>
-          <option value="5">5 - like it very much.</option>
+          <option value={question2.option1}>{question2.option1}</option>
+          <option value={question2.option2}>{question2.option2}</option>
+          <option value={question2.option3}>{question2.option3}</option>
+          <option value={question2.option4}>{question2.option4}</option>
+          <option value={question2.option5}>{question2.option5}</option>
         </select>
       </div>
 
@@ -50,32 +80,28 @@ const Feedback = () => {
           {question3}
         </label>
 
-        <select name="question3" id="question3">
-          <option value="-"> </option>
-          <option value="1">1 - very slow.</option>
-          <option value="2">2 - slow</option>
-          <option value="3">3 - can't decide</option>
-          <option value="4">4 - it's ok.</option>
-          <option value="5">5 - it works fast.</option>
-        </select>
+        <input
+          name="question3"
+          id="comment"
+          placeholder="Enter your comment here"
+          onChange={handleAnswer3}
+          value={answer3}
+        />
       </div>
 
       <div className={styles.formControl}>
-        <label htmlFor="question3" id="question4">
-          {question4}
-        </label>
-
-        <textarea name="comment" id="comment" placeholder="Enter your comment here"></textarea>
-      </div>
-
-      <div className={styles.formControl}>
-        <label htmlFor="question5">{question5}</label>
-
-        <textarea name="question5" id="cquestion5" placeholder="Enter your comment here"></textarea>
+        <label htmlFor="question4">{question4}</label>
+        <input
+          name="question4"
+          id="question4"
+          placeholder="Enter your comment here"
+          onChange={handleAnswer4}
+          value={answer4}
+        />
       </div>
 
       <button type="submit" value="submit">
-        Submit
+        {SUMBIT_LABEL}
       </button>
     </form>
   );
--- src/pages/Feedback/feedbackQuestions.js
+++ src/pages/Feedback/feedbackQuestions.js
@@ -2,13 +2,28 @@ export const title = 'Your Feedback Is Important.';
 
 export const titleSecondary = 'Please Answer The Following Questions.';
 
-export const question1 = 'How often do you use the application?';
+// Question 1
+export const question1 = {
+  question: 'How often do you use the application?',
+  option1: 'Once per month',
+  option2: 'Once or twice per week',
+  option3: 'Everyday',
+  option4: 'Never'
+};
 
-export const question2 = 'On a scale of 1 to 5, rate the interface of the application.';
+// Question 2
+export const question2 = {
+  question: 'On a scale of 1 to 5, rate the interface of the application.',
+  option1: "1 - don't like it at all.",
+  option2: '2 - it could be better',
+  option3: "3 - can't decide",
+  option4: '4 - like it.',
+  option5: '5 - like it very much'
+};
 
-export const question3 = 'On a scale of 1 to 5, rate the loading speed of the application.';
-
-export const question4 =
+// Question 3
+export const question3 =
   'Are there any features that you think you need but are missing in the application? Please describe.';
 
-export const question5 = 'Any comments or suggestions.';
+// Question 4
+export const question4 = 'Any comments or suggestions.';
