e31fdbbe9bf8cd42274f0f63540a3355ede5a932
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-10-14 18:31:01 +0300

    redux implemented

--- src/routes/Feedback.jsx
+++ src/routes/Feedback.jsx
@@ -3,28 +3,31 @@ import Input from '../UI/input/Input';
 import Button from '../UI/button/Button';
 import styles from '../styles.scss';
 import e from 'cors';
+import { useDispatch, useSelector } from 'react-redux';
+import { setComment, setUserName } from '../redux/features/commentSlice';
 
 function Feedback() {
-  const [userName, setUserName] = useState('');
-  const [comment, setComment] = useState('');
   const comments = JSON.parse(localStorage.getItem('comments')) || [];
-  function addFeedBack(e) {
+  const dispatch = useDispatch();
+  const userName = useSelector(state => state.comment.userName);
+  const comment = useSelector(state => state.comment.comment);
+  function addFeedback(e) {
     e.preventDefault();
     comments.push({ name: userName, comment: comment });
     localStorage.setItem('comments', JSON.stringify(comments));
-    setComment('');
-    setUserName('');
+    dispatch(setComment(''));
+    dispatch(setUserName(''));
   }
   return (
     <>
-      <form onSubmit={e => addFeedBack(e)} className={styles.formFeedback}>
+      <form onSubmit={addFeedback} className={styles.formFeedback}>
         <Input
-          onChange={e => setUserName(e.target.value)}
+          onChange={e => dispatch(setUserName(e.target.value))}
           placeholder="Your name"
           value={userName}
         />
         <textarea
-          onChange={e => setComment(e.target.value)}
+          onChange={e => dispatch(setComment(e.target.value))}
           placeholder="Your comment"
           type="text"
           value={comment}
@@ -34,7 +37,7 @@ function Feedback() {
       <div className={styles.comments}>
         {comments.map((comment, id) => (
           <p key={id}>
-            <span>{comment.name}</span>: "{comment.comment}"
+            <span>@{comment.name}</span>: "{comment.comment}"
           </p>
         ))}
       </div>
