307316dcb70f90074bff86bff627ea4f6b112402
Igor Silvestru <igor.silvestru.work@gmail.com>
2022-10-14 16:17:56 +0300

    implementing LS in comments

--- src/routes/Feedback.jsx
+++ src/routes/Feedback.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../UI/input/Input';
+import Button from '../UI/button/Button';
 import styles from '../styles.scss';
+import e from 'cors';
 
 function Feedback() {
+  const [userName, setUserName] = useState('');
+  const [comment, setComment] = useState('');
+  const comments = JSON.parse(localStorage.getItem('comments')) || [];
+  function addFeedBack(e) {
+    e.preventDefault();
+    comments.push({ name: userName, comment: comment });
+    localStorage.setItem('comments', JSON.stringify(comments));
+    setComment('');
+    setUserName('');
+  }
   return (
-    <form className={styles.formFeedback}>
-      <Input placeholder="Your name" />
-      <textarea placeholder="Your comment" type="text" />
-    </form>
+    <>
+      <form onSubmit={e => addFeedBack(e)} className={styles.formFeedback}>
+        <Input
+          onChange={e => setUserName(e.target.value)}
+          placeholder="Your name"
+          value={userName}
+        />
+        <textarea
+          onChange={e => setComment(e.target.value)}
+          placeholder="Your comment"
+          type="text"
+          value={comment}
+        />
+        <Button type="submit" text="Submit" />
+      </form>
+      <div className={styles.comments}>
+        {comments.map((comment, id) => (
+          <p key={id}>
+            <span>{comment.name}</span>: "{comment.comment}"
+          </p>
+        ))}
+      </div>
+    </>
   );
 }
 export default Feedback;
