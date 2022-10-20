96a888b561eb68adf0c26183844c78a17e3fab84
Stefan Atanasov <StefanAtanasov@coherentsolutions.com>
2022-10-20 15:34:39 +0300

    Made each feedback to be saved to local storage and implemented redux

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -3,6 +3,11 @@ import * as styles from '../../styles/HomePage.module.css';
 import { Nav } from './Nav/Nav.js';
 
 function HomePage(props) {
+  if (localStorage.getItem('userInput') === null) {
+    const userInput = {};
+    localStorage.setItem('userInput', JSON.stringify(userInput));
+  }
+
   return <Nav />;
 }
 
