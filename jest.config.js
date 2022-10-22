aa677244ddca952017a124ff49840637a4f61ab9
Andrei Shulha <AndreiShulha@coherentsolutions.com>
2022-10-22 15:54:36 +0300

    del comments

--- jest.config.js
+++ jest.config.js
@@ -7,34 +7,3 @@ module.exports = {
     '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules'
   }
 };
-
-//   transform: {
-//     '^.+\\.ts?$': 'ts-jest'
-//   },
-// setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-
-// testEnvironment: 'node',
-// transformIgnorePatterns: ['<rootDir>/node_modules/'],
-// globals: {
-//   'ts-jest': {
-//       tsconfig: '<rootDir>/tsconfig.json',
-//   },
-// },
-
-//   "modulePaths": [
-//     "<rootDir>/src/"
-//   ],
-//   "setupFilesAfterEnv": [
-//     "<rootDir>/src/setupTests.ts"
-//   ],
-//   "moduleNameMapper": {
-//     "chart.js": "<rootDir>/node_modules/chart.js/dist/chart.mjs"
-//   },
-//   "preset": "ts-jest",
-//   "testEnvironment": "node",
-//   "transform": {
-//     "^.+\\.ts?$": "ts-jest"
-//   },
-//   "transformIgnorePatterns": [
-//     "<rootDir>/node_modules/"
-//   ]
