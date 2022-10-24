9471b84387fa3af2132552816ef2194eed670994
ViliusGaizutis <viliusgaizutis@coherentsolutions.com>
2022-10-24 16:42:46 +0300

    Set up React Testing Library with Jest and ESLint

---
+++ client/jest.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: false,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/types/**/*.ts',
+    '!src/**/index.ts',
+    '!src/tests/**/*.{js,jsx,ts,tsx}',
+    '!src/**/stories/**/*.{js,jsx,ts,tsx}',
+    '!src/storybook-utils/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+    '!src/**/*.stories.{js,jsx,ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  preset: 'ts-jest',
+  resetMocks: true,
+  roots: ['<rootDir>/src'],
+  setupFiles: ['dotenv/config'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ]
+};
